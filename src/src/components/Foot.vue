942035ede0337f66116c116ffb93b0af98e07a86
addshore <addshorewiki@gmail.com>
2019-07-13 15:00:14 -0600

    ui, vuetify Foot.vue

--- src/src/components/Foot.vue
+++ src/src/components/Foot.vue
@@ -1,32 +1,32 @@
 <template>
   <footer>
-    <div class="wrapper">
-      <div class="md-layout">
-        <div class="md-layout-item">
+    <v-container grid-list-xl>
+      <v-layout align-start justify-space-between>
+        <v-flex xs3>
           <h5>Explore</h5>
           <ul>
             <li>News</li>
             <li>Discover</li>
           </ul>
-        </div>
-        <div class="md-layout-item">
+        </v-flex>
+        <v-flex xs3>
           <h5>Community</h5>
           <ul>
             <li>Mediawiki</li>
             <li>Wikibase</li>
             <li>Wikimedia</li>
           </ul>
-        </div>
-        <div class="md-layout-item">
+        </v-flex>
+        <v-flex xs3>
           <h5>Company</h5>
           <ul>
             <li>About Us</li>
             <li>Privacy</li>
             <li>Terms of Service</li>
           </ul>
-        </div>
-      </div>
-    </div>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </footer>
 </template>
 
@@ -36,21 +36,7 @@ export default {
 }
 </script>
 
-<style lang="css" scoped>
-
-  .md-layout{
-    display: flex;
-    max-width:1152px;
-    align-items: center;
-    text-align: left;
-    margin: 0 auto;
-  }
-
-  .wrapper{
-    padding-left: 10px;
-    padding-right: 10px;
-    margin: 0 auto;
-  }
+<style scoped>
 
   ul {
     list-style: none;
