e8576620238343691e537bd84d69f38dac9ddffd
addshore <addshorewiki@gmail.com>
2019-08-14 19:05:32 -0600

    api and ui, wikimanagers start
    Create a wikimanager on wiki creation
    Only list wikis you are a manager of
    List wikimanagers on wiki manage page

--- src/src/components/Pages/ManageWiki/ManageWiki.vue
+++ src/src/components/Pages/ManageWiki/ManageWiki.vue
@@ -2,14 +2,28 @@
     <v-content>
       <v-container fluid fill-height>
         <v-layout justify-center>
-          <v-flex xs6>
+          <v-flex xs8>
             <template v-if="id != 0">
               <v-card>
                 <v-toolbar dark color="primary">
                   <v-toolbar-title>Manage Wiki {{id}}</v-toolbar-title>
                 </v-toolbar>
                 <v-card-text>
-                  This component has not yet been implemented. Managing coming soon...
+                  <h3>Managers</h3>
+
+                  <v-list-tile v-for="manager in managers" :key="manager.email" :manager="manager">
+                    <v-list-tile-content>
+                      <div class="fullwidth">
+                        {{manager.email}}
+                        <!--
+                        <div class="rightfloat">
+                          <v-btn icon small class="nomargin" @click="deleteInvitationCode(invite.code)"><v-icon>delete</v-icon></v-btn>
+                        </div>
+                      -->
+                      </div>
+                  </v-list-tile-content>
+                  </v-list-tile>
+
                 </v-card-text>
               </v-card>
             </template>
@@ -34,13 +48,26 @@ export default {
   name: 'ManageWiki',
   data () {
     return {
-      id: 0
+      id: 0,
+      managers: []
     }
   },
   computed: {},
   created () {
     this.id = this.$route.params.id
     // TODO get the data about the wiki if the user can see it...
+    this.$http.post(
+      '/wiki/managers/list',
+      { wiki: this.id },
+      {headers: {'Authorization': localStorage.auth}}
+    )
+      .then(request => this.buildManagersList(request.data.data))
+      .catch(() => { alert('Failed to retrieve managers!') })
+  },
+  methods: {
+    buildManagersList (data) {
+      this.managers = data
+    },
   }
 }
 </script>
