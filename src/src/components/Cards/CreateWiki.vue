2f11d1c782f1e39a3818f41e37f9856bc5c3d959
addshore <addshorewiki@gmail.com>
2019-08-17 10:28:24 -0600

    ui, split createwiki card out

---
+++ src/src/components/Cards/CreateWiki.vue
@@ -0,0 +1,157 @@
+<template>
+  <v-card class="elevation-12">
+    <v-toolbar dark color="primary">
+      <v-toolbar-title>{{title}}</v-toolbar-title>
+    </v-toolbar>
+    <v-card-text>
+      <v-form>
+
+        <h3>Site Name</h3>
+        <p>This will appear in your page titles and can be changed at any time.</p>
+        <v-text-field
+        id="inputSiteName"
+        prepend-icon="person"
+        name="sitename"
+        label="E.g., Goat Collective"
+        required
+        v-model="sitename"
+        :disabled="inFlight"
+        :error-messages="error['sitename']"
+        />
+
+        <h3>Site Address</h3>
+        <p>Choose a subdomain for your site to be accessed at.</p>
+        <v-text-field
+        id="inputSubdomain"
+        prepend-icon="person"
+        name="subdomain"
+        label="E.g., goat-collective"
+        required
+        v-model="subdomain"
+        suffix=".wiki.opencura.com"
+        :disabled="inFlight"
+        :error-messages="error['siteaddress']"
+        />
+
+        <h3>Terms of Use</h3>
+        <v-checkbox
+        required
+        v-model="terms"
+        :disabled="inFlight"
+        :error-messages="error['terms']"
+        >
+          <template v-slot:label>
+            <div>
+              I agree to the
+              <v-tooltip bottom>
+                <template v-slot:activator="{ on }">
+                  <a
+                    target="_blank"
+                    href="/terms-of-use"
+                    @click.stop
+                    v-on="on"
+                  >
+                    Terms of Use
+                  </a>
+                </template>
+                Opens in new window
+              </v-tooltip>
+              .
+            </div>
+          </template>
+        </v-checkbox>
+
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn
+      @click="createwiki"
+      color="primary"
+      :disabled="inFlight"
+      >
+      {{buttonText}}
+    </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'CreateWiki',
+  props: [
+    'title',
+    'buttonText'
+  ],
+  computed: {
+    ...mapGetters({ currentUser: 'currentUser' })
+  },
+  data () {
+    return {
+      sitename: '',
+      subdomain: '',
+      terms: false,
+      hasError: false,
+      error: [],
+      inFlight: false
+    }
+  },
+  created () {
+    this.checkCurrentLogin()
+  },
+  updated () {
+    this.checkCurrentLogin()
+  },
+  methods: {
+    createwiki () {
+      this.inFlight = true
+      this.hasError = false
+      this.error = []
+
+      if (!this.terms) {
+        this.hasError = true
+        this.error['terms'] = 'You must accept the Terms of Service.'
+      }
+
+      if (this.hasError) {
+        this.inFlight = false
+        return
+      }
+
+      this.$http.post(
+        '/wiki/create',
+        {
+          domain: this.subdomain + '.wiki.opencura.com',
+          sitename: this.sitename
+        },
+        {headers: {'Authorization': localStorage.auth}}
+      )
+        .then(request => this.createSuccess(request))
+        .catch((error) => this.createFail(error))
+    },
+    createSuccess (req) {
+      this.hasError = false
+      this.error = []
+      //this.$router.replace(this.$route.query.redirect || '/wikis/manage/' + req.data.data.id)
+      this.$router.replace('/wikis/manage/' + req.data.data.id)
+    },
+    createFail () {
+      this.hasError = true
+      this.error['sitename'] = 'Creation failed!'
+      this.error['siteaddress'] = 'Creation failed!'
+      this.error['terms'] = 'Creation failed!'
+      this.inFlight = false
+    },
+    checkCurrentLogin () {
+      if (!this.currentUser) {
+        this.$router.replace(this.$route.query.redirect || '/')
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
