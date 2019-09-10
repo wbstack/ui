d3d605af5c3bc069173e075429f10a69eb2d1593
addshore <addshorewiki@gmail.com>
2019-09-10 13:49:22 -0500

    ui, add resourcetier selection to add wiki form

--- src/src/components/Cards/CreateWiki.vue
+++ src/src/components/Cards/CreateWiki.vue
@@ -33,6 +33,16 @@
         :error-messages="error['siteaddress']"
         />
 
+        <h3>Resource Tier</h3>
+        <p>What resource level would you like to allocate to your site?</p>
+        <p>During Alpha only the free tier is supported.</p>
+        <v-select
+          v-model="freeSelected"
+          :items="resourceTiers"
+          label="Resource tier"
+          solo
+        ></v-select>
+
         <h3>Terms of Use</h3>
         <v-checkbox
         required
@@ -94,6 +104,28 @@ export default {
     return {
       sitename: '',
       subdomain: '',
+      freeSelected: 'free',
+      resourceTiers: [
+        {
+          text: 'Free',
+          value: 'free',
+        },
+        {
+          text: 'Personal',
+          value: 'personal',
+          disabled: true
+        },
+        {
+          text: 'Premium',
+          value: 'premium',
+          disabled: true
+        },
+        {
+          text: 'Pro',
+          value: 'pro',
+          disabled: true
+        }
+      ],
       terms: false,
       hasError: false,
       error: [],
