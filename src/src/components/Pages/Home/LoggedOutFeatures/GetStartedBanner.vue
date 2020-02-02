88c761496765dfd84c84af71cad66ffd2f72a01c
addshore <addshorewiki@gmail.com>
2020-02-02 14:29:14 +0000

    ui, update blog list on mainpage (missed in last commit)

--- src/src/components/Pages/Home/LoggedOutFeatures/GetStartedBanner.vue
+++ src/src/components/Pages/Home/LoggedOutFeatures/GetStartedBanner.vue
@@ -13,6 +13,7 @@
             <v-flex>
               <h3>Blog posts:</h3>
               <ul>
+                <li><a href="https://addshore.com/2020/01/wbstack-infrastructure/">Infrastructure oerview</a></li>
                 <li><a href="https://addshore.com/2019/11/wbstack-november-review">November review</a></li>
                 <li><a href="https://addshore.com/2019/11/an-introduction-to-wbstack/">October introduction</a></li>
               </ul>
