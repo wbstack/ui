2a453dec9bbb2320833439e6be13dd9995304b37
toan <tobias.andersson@wikimedia.de>
2021-03-23 17:17:49 +0100

    Add federatedproperties modal

--- src/components/Pages/ManageWiki/Tabs/ManageFeatures.vue
+++ src/components/Pages/ManageWiki/Tabs/ManageFeatures.vue
@@ -1,17 +1,25 @@
 <template>
   <v-content>
-      Feature Settings
+    <FederatedProperties :wikiId="this.wikiId" />
   </v-content>
 </template>
 
 <script>
+import FederatedProperties from '../Features/FederatedProperties.vue'
+
 export default {
   name: 'ManageFeatures',
-  components: {
+  components: { FederatedProperties },
+  data () {
+    return {
+      wikiId: 0
+    }
+  },
+  created () {
+    this.wikiId = this.$route.params.id
   }
 }
 </script>
 
 <style scoped>
-
 </style>
