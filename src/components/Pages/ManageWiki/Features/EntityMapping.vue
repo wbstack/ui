effefee2e0e3e68effeda7c5e361ed1c906ad54a
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2021-03-24 12:31:01 +0100

    Add settings sections for mapping entities

---
+++ src/components/Pages/ManageWiki/Features/EntityMapping.vue
@@ -0,0 +1,50 @@
+<template>
+<div>
+	<v-card
+		elevation="1"
+		outlined
+	>
+	<v-card-title>Mapping Properties to Wikidata</v-card-title>
+	<v-card-text>
+        Some tools assume properties with special meanings under certain identifiers.
+		Here, you can map a property on your Wikibase instance to a property on Wikidata.
+		E.g. if a tool needs instanceOf (P31 on Wikidata) but the instanceOf property is P4 
+		on your Wikibase you can create a mapping between them.
+      </v-card-text>
+	  <PropertiesTable/>
+	  
+	</v-card>
+	<v-divider class="space"></v-divider>
+	<v-card
+		elevation="1"
+		outlined
+	>
+	<v-card-title>Mapping Items to Wikidata</v-card-title>
+	<v-card-text>
+        Some tools assume items with special meanings under certain identifiers. 
+		Here, you can map a item on your Wikibase instance to an item on Wikidata.
+      </v-card-text>
+	  <ItemsTable/>
+	</v-card>
+</div>
+</template>
+
+<script>
+import PropertiesTable from './PropertiesTable.vue';
+import ItemsTable from './ItemsTable.vue';
+
+export default {
+  name: 'EntityMapping',
+  components: { PropertiesTable, ItemsTable },
+  props: [],
+  methods: {
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.space {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/Pages/ManageWiki/Features/PropertiesTable.vue
@@ -0,0 +1,224 @@
+<template>
+  <v-data-table
+	:headers="headers"
+	:items="properties"
+	sort-by="wikidata"
+	class="elevation-1"
+  >
+	<template v-slot:top>
+	  <v-toolbar
+		flat
+	  >
+		<v-toolbar-title>Properties</v-toolbar-title>
+		<v-divider
+		  class="mx-4"
+		  inset
+		  vertical
+		></v-divider>
+		<v-spacer></v-spacer>
+		<v-dialog
+		  v-model="dialog"
+		  max-width="500px"
+		>
+		  <template v-slot:activator="{ on, attrs }">
+			<v-btn
+			  color="primary"
+			  dark
+			  class="mb-2"
+			  v-bind="attrs"
+			  v-on="on"
+			>
+			  Add New Mapping
+			</v-btn>
+		  </template>
+		  <v-card>
+			<v-card-title>
+			  <span class="headline">{{ formTitle }}</span>
+			</v-card-title>
+
+			<v-card-text>
+			  <v-container>
+				<v-row>
+				  <v-col
+					cols="12"
+					sm="6"
+					md="4"
+				  >
+					<v-text-field
+					  v-model="editedItem.local"
+					  label="My Wikibase (Your Instance)"
+					></v-text-field>
+				  </v-col>
+				  <v-col
+					cols="12"
+					sm="6"
+					md="4"
+				  >
+					<v-text-field
+					  v-model="editedItem.wikidata"
+					  label="Wikidata"
+					></v-text-field>
+				  </v-col>
+				</v-row>
+			  </v-container>
+			</v-card-text>
+
+			<v-card-actions>
+			  <v-spacer></v-spacer>
+			  <v-btn
+				color="blue darken-1"
+				text
+				@click="close"
+			  >
+				Cancel
+			  </v-btn>
+			  <v-btn
+				color="blue darken-1"
+				text
+				@click="save"
+			  >
+				Save
+			  </v-btn>
+			</v-card-actions>
+		  </v-card>
+		</v-dialog>
+		<v-dialog v-model="dialogDelete" max-width="500px">
+		  <v-card>
+			<v-card-title class="headline">Are you sure you want to delete this mapping?</v-card-title>
+			<v-card-actions>
+			  <v-spacer></v-spacer>
+			  <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+			  <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+			  <v-spacer></v-spacer>
+			</v-card-actions>
+		  </v-card>
+		</v-dialog>
+	  </v-toolbar>
+	</template>
+	<template v-slot:[`item.actions`]="{ item }">
+	  <v-icon
+	  	small
+		class="mr-2"
+		@click="editItem(item)"
+	  >
+		edit
+	  </v-icon>
+	  
+	  <v-icon
+	  	small
+		@click="deleteItem(item)"
+	  >
+		delete
+	  </v-icon>
+	</template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+	name: 'EntityTable',
+	data: () => ({
+      dialog: false,
+      dialogDelete: false,
+      headers: [
+        {
+          text: 'My Wikibase (Your Instance)',
+          align: 'start',
+          sortable: true,
+          value: 'local',
+        },
+        { text: 'Wikidata', value: 'wikidata' },
+        { text: 'Actions', value: 'actions', sortable: false },
+      ],
+      properties: [],
+      editedIndex: -1,
+      editedItem: {
+        local: '',
+        wikidata: '',
+      },
+      defaultItem: {
+        local: '',
+        wikidata: '',
+      },
+    }),
+
+    computed: {
+      formTitle () {
+        return this.editedIndex === -1 ? 'New Mapping' : 'Edit Mapping'
+      },
+    },
+
+    watch: {
+      dialog (val) {
+        val || this.close()
+      },
+      dialogDelete (val) {
+        val || this.closeDelete()
+      },
+    },
+
+    created () {
+      this.initialize()
+    },
+
+    methods: {
+      initialize () {
+        this.properties = [
+          {
+            local: 'No Mapping',
+            wikidata: 'P31 (Instance of)',
+          },
+          {
+            local: 'No Mapping',
+            wikidata: 'P10 (Subclass of)',
+          },
+        ]
+      },
+
+      editItem (item) {
+        this.editedIndex = this.properties.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+
+      deleteItem (item) {
+        this.editedIndex = this.properties.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialogDelete = true
+      },
+
+      deleteItemConfirm () {
+        this.properties.splice(this.editedIndex, 1)
+        this.closeDelete()
+      },
+
+      close () {
+        this.dialog = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+
+      closeDelete () {
+        this.dialogDelete = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+
+      save () {
+        if (this.editedIndex > -1) {
+          Object.assign(this.properties[this.editedIndex], this.editedItem)
+        } else {
+          this.properties.push(this.editedItem)
+        }
+        this.close()
+      },
+    },
+}
+</script>
+
+<style lang="css" scoped>
+</style>
