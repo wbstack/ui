2d08109c2fbe8e931b7c10bf1edb5c7146bb51d5
Tonina <tonina.zhelyazkova@wikimedia.de>
2021-03-26 16:46:21 +0100

    Add feedback banner (#83)
    * Add feedback banner
    
    * Update src/components/Pages/ManageWiki/Tabs/ManageFeatures.vue
    
    Co-authored-by: addshore <addshore@users.noreply.github.com>
    
    * Update src/components/Pages/ManageWiki/Tabs/ManageFeatures.vue
    
    Co-authored-by: addshore <addshore@users.noreply.github.com>
    
    Co-authored-by: addshore <addshore@users.noreply.github.com>

--- src/components/Pages/ManageWiki/Tabs/ManageFeatures.vue
+++ src/components/Pages/ManageWiki/Tabs/ManageFeatures.vue
@@ -1,6 +1,13 @@
 <template>
   <v-main>
     <div class="space"></div>
+    <v-banner elevation="6" icon="open_in_new">
+       <a href="https://docs.google.com/forms/d/e/1FAIpQLSfXI4SmbNVV0cF5gxpUalHYhvmrRudbALr5fmTjMxLnUihXug/viewform?usp=sf_link" target="_blank">
+          Please give Wikimedia Germany feedback
+        </a>
+        on Federated Properties to help direct future development efforts.
+    </v-banner>
+    <div class="space"></div>
     <EntityMapping :wikiId="wikiId"/>
     <div class="space"></div>
     <FederatedProperties :wikiId="wikiId" />
