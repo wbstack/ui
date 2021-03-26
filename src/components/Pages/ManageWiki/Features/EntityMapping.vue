12a388ca2e41ebbcad217d48c8d7375187b8cfc6
addshore <addshore@users.noreply.github.com>
2021-03-26 09:11:30 +0000

    Refactor mapping tables

--- src/components/Pages/ManageWiki/Features/EntityMapping.vue
+++ src/components/Pages/ManageWiki/Features/EntityMapping.vue
@@ -1,10 +1,6 @@
 <template>
 <div>
-    <v-card
-        elevation="1"
-        class="padding"
-        outlined
-    >
+    <v-card elevation="1" class="padding" outlined>
         <v-card-title>Mapping Properties to Wikidata</v-card-title>
         <v-card-text>
             Some tools assume properties with special meanings under certain identifiers.
@@ -12,33 +8,28 @@
             E.g. if a tool needs instanceOf (P31 on Wikidata) but the instanceOf property is P4
             on your Wikibase you can create a mapping between them.
         </v-card-text>
-        <PropertiesTable/>
+        <EntityTable mappingName="properties" :entityValidation="/^P\d+$/" entityName="Property" storeAction="setPropertyMapping" />
     </v-card>
 
     <v-divider class="space"></v-divider>
 
-    <v-card
-        elevation="1"
-        class="padding"
-        outlined
-    >
+    <v-card elevation="1" class="padding" outlined>
         <v-card-title>Mapping Items to Wikidata</v-card-title>
         <v-card-text>
             Some tools assume items with special meanings under certain identifiers.
             Here, you can map a item on your Wikibase instance to an item on Wikidata.
         </v-card-text>
-        <ItemsTable/>
+        <EntityTable mappingName="items" :entityValidation="/^Q\d+$/" entityName="Item" storeAction="setItemMapping" />
     </v-card>
 </div>
 </template>
 
 <script>
-import PropertiesTable from './PropertiesTable.vue'
-import ItemsTable from './ItemsTable.vue'
+import EntityTable from '../Features/EntityTable.vue';
 
 export default {
   name: 'EntityMapping',
-  components: { PropertiesTable, ItemsTable },
+  components: { EntityTable },
   props: ['wikiId']
 }
 </script>
