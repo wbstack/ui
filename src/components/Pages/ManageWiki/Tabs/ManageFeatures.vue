1df32034babebaab505f2d4262d4fe2e9d33802e
Tonina <tonina.zhelyazkova@wikimedia.de>
2021-03-26 16:32:17 +0100

    Update src/components/Pages/ManageWiki/Tabs/ManageFeatures.vue
    Co-authored-by: addshore <addshore@users.noreply.github.com>

--- src/components/Pages/ManageWiki/Tabs/ManageFeatures.vue
+++ src/components/Pages/ManageWiki/Tabs/ManageFeatures.vue
@@ -3,7 +3,7 @@
     <div class="space"></div>
     <v-banner elevation="6" icon="open_in_new">
        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfXI4SmbNVV0cF5gxpUalHYhvmrRudbALr5fmTjMxLnUihXug/viewform?usp=sf_link" target="_blank">
-          Please give us feedback
+          Please give Wikimedia Germany feedback
         </a>
         on Federated Properties to help direct future development efforts.
     </v-banner>
