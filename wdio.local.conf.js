1d23243ce5e5d38bbf048f27cadc44d6f04216b9
toan <tobias.andersson@wikimedia.de>
2021-03-26 18:09:18 +0100

    add screenshot after test
    - adds wdio-mediawiki for screenshot function

--- wdio.local.conf.js
+++ wdio.local.conf.js
@@ -1,4 +1,5 @@
 const { config } = require('./wdio.shared.conf')
+const saveScreenshot = require( 'wdio-mediawiki' ).saveScreenshot;
 
 exports.config = {
   /**
@@ -17,5 +18,17 @@ exports.config = {
         args: ['-headless']
       },
     }
-  ]
+  ],
+
+	// Setting this enables automatic screenshots
+	screenshotPath: process.env.LOG_DIR || __dirname + '/tests/e2e/logs',
+
+  /**
+   * Function to be executed after a test (in Mocha/Jasmine) ends.
+   */
+  afterTest: function (test, context, { error, result, duration, passed, retries }) {
+    const filePath = saveScreenshot( test.title.replace(/ /g, '_') );
+    console.log( '\n\tScreenshot: ' + filePath + '\n' );
+  },
+
 }
