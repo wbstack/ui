cd5acb4417d555490646498c134d9123bf058db8
addshore <addshore@users.noreply.github.com>
2021-03-26 15:23:26 +0000

    Slightly expand existing e2e test (#63)

--- src/components/Layout/Navbar.vue
+++ src/components/Layout/Navbar.vue
@@ -9,13 +9,13 @@
     <v-toolbar-items>
       <template v-if="isLoggedIn">
         <v-btn class="text-none no-button-pointer-events" text> Hi {{ currentUser.email }}</v-btn>
-        <v-btn text to="/dashboard">Dashboard</v-btn>
-        <v-btn text to="/user">Account</v-btn>
-        <v-btn text to="/logout">Logout</v-btn>
+        <v-btn id="nav-dashboard" text to="/dashboard">Dashboard</v-btn>
+        <v-btn id="nav-user" text to="/user">Account</v-btn>
+        <v-btn id="nav-logout" text to="/logout">Logout</v-btn>
       </template>
       <template v-if="!isLoggedIn">
-        <v-btn text to="/login">Login</v-btn>
-        <v-btn text color="primary" to="/create-account">Get Started</v-btn>
+        <v-btn id="nav-login" text to="/login">Login</v-btn>
+        <v-btn id="nav-create-account" ext color="primary" to="/create-account">Get Started</v-btn>
       </template>
     </v-toolbar-items>
   </v-toolbar>
