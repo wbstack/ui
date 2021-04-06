c3b245b97b7217ecbe1858bc6071a2fda4b3796a
addshore <addshorewiki@gmail.com>
2021-04-06 19:42:49 +0100

    Alter block sizings a little more

--- src/components/Pages/Home/LoggedOutFeatures/WhatCanYouDo.vue
+++ src/components/Pages/Home/LoggedOutFeatures/WhatCanYouDo.vue
@@ -7,38 +7,34 @@
       <p class="text-center">WbStack allows you to easily curate data in your own Wikibase instance, without worrying about the technical side of things.</p>
     </v-col></v-row>
     <v-row class="lists">
-      <!-- <v-col> -->
-        <!-- START blocks side by side -->
-          <v-row align="stretch" justify="space-between">
-            <v-col cols="12" sm="4">
-              <v-card class="what-can-you-do__card">
-                <v-card-title><h3>Create a Wikibase</h3></v-card-title>
-                <v-card-text>
-                Get set up in under a minute, with no technical knowledge, get automatic upgrades and much more.
-                </v-card-text>
-              </v-card>
-            </v-col>
-            <v-col cols="12" sm="4">
-              <v-card class="what-can-you-do__card">
-                <v-card-title><h3>Build a community</h3></v-card-title>
-                <v-card-text>Allow people to find your data, in the standard Wikibase format, and help you curate it.</v-card-text>
-              </v-card>
-            </v-col>
-            <v-col cols="12" sm="4">
-              <v-card class="what-can-you-do__card">
-                <v-card-title><h3>Link to other data</h3></v-card-title>
-                <v-card-text>Link to other open data sets and become part of the <a target="_blank" rel="noopener noreferrer" href="https://lod-cloud.net/">Linked Open Data Cloud</a>.</v-card-text>
-              </v-card>
-            </v-col>
-            <v-col cols="12" sm="4">
-              <v-card class="what-can-you-do__card">
-                <v-card-title><h3>Query everything</h3></v-card-title>
-                <v-card-text>Data and input managed by <a target="_blank" rel="noopener noreferrer" href="https://www.mediawiki.org/wiki/MediaWiki" >MediaWiki</a>, Triples and querying handled in <a target="_blank" rel="noopener noreferrer" href="https://www.wikidata.org/wiki/Wikidata:SPARQL_tutorial" >SPARQL</a> via Blazegraph.</v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        <!-- END blocks side by side -->
-      <!-- </v-col> -->
+      <v-row align="stretch">
+        <v-col cols="12" lg="3" md="6" sm="6">
+          <v-card class="what-can-you-do__card">
+            <v-card-title><h3>Create a Wikibase</h3></v-card-title>
+            <v-card-text>
+            Get set up in under a minute, with no technical knowledge, get automatic upgrades and much more.
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="12" lg="3" md="6" sm="6">
+          <v-card class="what-can-you-do__card">
+            <v-card-title><h3>Build a community</h3></v-card-title>
+            <v-card-text>Allow people to find your data, in the standard Wikibase format, and help you curate it.</v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="12" lg="3" md="6" sm="6">
+          <v-card class="what-can-you-do__card">
+            <v-card-title><h3>Link to other data</h3></v-card-title>
+            <v-card-text>Link to other open data sets and become part of the <a target="_blank" rel="noopener noreferrer" href="https://lod-cloud.net/">Linked Open Data Cloud</a>.</v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="12" lg="3" md="6" sm="6">
+          <v-card class="what-can-you-do__card">
+            <v-card-title><h3>Query everything</h3></v-card-title>
+            <v-card-text>Data and input managed by <a target="_blank" rel="noopener noreferrer" href="https://www.mediawiki.org/wiki/MediaWiki" >MediaWiki</a>, Triples and querying handled in <a target="_blank" rel="noopener noreferrer" href="https://www.wikidata.org/wiki/Wikidata:SPARQL_tutorial" >SPARQL</a> via Blazegraph.</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
       <!-- TODO uncomment below when registration is allowed -->
       <!--
       <v-flex>
