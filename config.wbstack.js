641576eb326b13c8825e9a6ecc30b11522b19bba
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2021-10-19 14:48:35 +0200

    Fix netlify previews

---
+++ config.wbstack.js
@@ -0,0 +1,8 @@
+// config.js will be mounted in the container in production
+
+const config = {
+  RECAPTCHA_KEY: '6LeHzbMUAAAAABjNp0vILaWr5ZeYHmteF7rGuZNV',
+  API_URL: 'https://www.wbstack.com/api',
+  SUBDOMAIN_SUFFIX: '.wiki.opencura.com',
+  MOCK_API: false,
+}
