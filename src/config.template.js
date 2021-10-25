d6192b7876ea6403358ce8410aa65e3442364a93
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2021-10-25 15:41:11 +0200

    Fix recaptcha site key config name

--- src/config.template.js
+++ src/config.template.js
@@ -2,7 +2,7 @@
 
 // keep in sync with src/config.js
 window.config = {
-  RECAPTCHA_KEY: '${RECAPTCHA_KEY}',
+  RECAPTCHA_SITE_KEY: '${RECAPTCHA_SITE_KEY}',
   API_URL: '${API_URL}',
   SUBDOMAIN_SUFFIX: '${SUBDOMAIN_SUFFIX}',
   MOCK_API: '${MOCK_API}'
