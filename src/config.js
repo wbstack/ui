c34cb6db202ec9db326dbdf3e7f7e41e70ad4fc1
Jakob Warkotsch <j.warkotsch@gmail.com>
2021-10-26 12:44:14 +0200

    Fix recaptcha site key config name (#265)

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // keep in sync with src/config.template.js
 export default {
-  RECAPTCHA_KEY: process.env.VUE_APP_RECAPTCHA_KEY,
+  RECAPTCHA_SITE_KEY: process.env.VUE_APP_RECAPTCHA_SITE_KEY,
   API_URL: process.env.VUE_APP_API_URL,
   SUBDOMAIN_SUFFIX: process.env.VUE_APP_SUBDOMAIN_SUFFIX,
   MOCK_API: process.env.VUE_APP_MOCK_API
