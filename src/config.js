763c14ca56d863f1f3287fcb78a7f366b8ae8090
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2022-02-16 17:47:35 +0000

    Fix API_MOCK environment variable

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ export default {
   RECAPTCHA_SITE_KEY: process.env.VUE_APP_RECAPTCHA_SITE_KEY,
   API_URL: process.env.VUE_APP_API_URL,
   SUBDOMAIN_SUFFIX: process.env.VUE_APP_SUBDOMAIN_SUFFIX,
-  MOCK_API: process.env.VUE_APP_MOCK_API
+  API_MOCK: process.env.VUE_APP_API_MOCK
 }
