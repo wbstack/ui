3e431e5e2e1acbf8657af894e820c0c9f631b1cb
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2022-03-23 15:51:48 +0000

    Adjust Landing Page
    Bug: T302866

--- src/components/Pages/Home/Home.vue
+++ src/components/Pages/Home/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <GetStartedBanner></GetStartedBanner>
+    <h2>Make your structured data available in our cloud</h2>
+    <p>If you've wanted to run your own Wikibase but don't have the infrastructure and support you'd need, Wikibase.cloud is for you. You provide the data – we provide a full-fledged and fully managed Wikibase. </p>
+    <h3>Coming soon</h3>
+    <p>Wikibase.cloud is in a closed beta, which means we're not yet accepting account requests. While you wait, consider joining the Wikibase community mailing list.</p>
+    
+    <h3>Still have questions?</h3>
+    <p>Drop us a line using our <a href="https://wikiba.se/contact/">contact form</a>.</p>
   </div>
 </template>
 
