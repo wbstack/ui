c6723557b352cf41c57e4cad09eccb2c1b668032
Dat <dat.nguyen@wikimedia.de>
2022-07-21 11:31:12 +0200

    Re-add waiting link to landing page

--- src/components/Pages/Home/Home.vue
+++ src/components/Pages/Home/Home.vue
@@ -3,7 +3,7 @@
     <h2>Make your structured data available in our cloud</h2>
     <p>If you've wanted to run your own Wikibase but don't have the infrastructure and support you'd need, Wikibase.cloud is for you. You provide the data – we provide a full-fledged and fully managed Wikibase. </p>
     <h3>Coming soon</h3>
-    <p>Wikibase.cloud is in a closed beta, which means we're not yet accepting account requests. While you wait, consider joining the Wikibase community <a href="https://lists.wikimedia.org/postorius/lists/wikibase-cloud.lists.wikimedia.org/">mailing list</a>.</p>
+    <p>Wikibase.cloud is in a closed beta, which means we're not yet accepting account requests. If you're interested in a Wikibase.cloud instance, sign up for early access to our closed beta <a href="https://lime.wikimedia.de/index.php/717538">here</a>. While you wait, consider joining the Wikibase community <a href="https://lists.wikimedia.org/postorius/lists/wikibase-cloud.lists.wikimedia.org/">mailing list</a>. </p>
 
     <h3>Still have questions?</h3>
     <p>Drop us a line using our <a href="https://wikiba.se/contact/">contact form</a>. Now is also a great time to get involved in the <a href="https://meta.wikimedia.org/wiki/Wikibase_Community_User_Group">Wikibase community</a>. </p>
