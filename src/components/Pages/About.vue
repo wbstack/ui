2d38032c45ecb2176643caeb777b30b78d173613
addshore <addshorewiki@gmail.com>
2022-09-05 12:18:12 +0100

    Remove old /about page content
    As with /contact, the about link in the footer now links
    to wikiba.se
    However the /about route still holds old content.
    So just redirect this route to the same place as the footer link
    for now.

--- src/components/Pages/About.vue
+++ src/components/Pages/About.vue
@@ -4,12 +4,9 @@
         <v-row justify="center">
           <v-col cols="8">
             <h1>About</h1>
-            <p>The WBStack alpha platform was released in late 2019.</p>
-            <p>It was open sourced in late 2020</p>
-            <p><a target="_blank" rel="noopener noreferrer" href="https://rhizome.org/">Rhizome</a> have funded WBStack hosting for the majority of this time.</p>
-            <p>In 2021 <a target="_blank" rel="noopener noreferrer" href="https://www.wikimedia.de/">Wikimedia Germany</a> started covering the costs of hosting.</p>
-            <p>You can read more about the platform and history on the <a target="_blank" rel="noopener noreferrer" href="https://wbstack.github.io/docs/">docs site</a> or in the blog posts below.</p>
-            <BlogPostList headerElement="h3" headerText="Blog Posts" ></BlogPostList>
+            <v-col>
+              <p>Redirecting to <a href="https://wikiba.se/about-us/">https://wikiba.se/about-us/</a></p>
+            </v-col>
           </v-col>
         </v-row>
       </v-container>
@@ -17,13 +14,11 @@
 </template>
 
 <script>
-import BlogPostList from '~/components/Content/BlogPostList'
 
 export default {
   name: 'About',
-  computed: {},
-  components: {
-    BlogPostList
+  created () {
+    window.location.href = 'https://wikiba.se/about-us/'
   }
 }
 </script>
