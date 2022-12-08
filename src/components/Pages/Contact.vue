0b0ee4a4f68276b524e862f4ac96afefb9e1fa67
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2022-12-08 14:42:11 +0000

    Fix responsive behaviour

--- src/components/Pages/Contact.vue
+++ src/components/Pages/Contact.vue
@@ -1,50 +1,49 @@
 <template>
     <v-main>
-      <v-container class="fill-height" >
-        <v-row>
-          <v-col cols="8">
-            <h1>Get in touch with the team</h1>
-            <p class="mt-5">Thank you for your interest. We really want to hear what you've got to say. </p>
-            <p>Have you got a question? First, check our <a href="https://www.mediawiki.org/wiki/Talk:Wikibase/FAQ" target="_blank">FAQ</a> to see if we've already answered it. We're constantly  updating it with new questions. if you don't find an answer there, feel free to use the contact form below. Thanks!</p>
-            <h3 class="mt-5">Want to connect with the Wikibase.cloud community?</h3>
-            <p>You can visit our <a href="https://t.me/joinchat/FgqAnxNQYOeAKmyZTIId9g" target="_blank">Telegram channel</a> or join our <a href="https://lists.wikimedia.org/postorius/lists/wikibase-cloud.lists.wikimedia.org/" target="_blank">mailing list</a>.</p>
-            <div>
-              <h2 class="mt-10 mb-2">Contact Us</h2>
-              <v-text-field
-                id="Name"
-                v-model="name"
-                :rules="[() => !!name || 'This field is required', v => v.length <= 300 || 'Max 300 characters']"
-                type="text"
-                label="Your Name"
-              />
-              <v-text-field
-                id="contactDetail"
-                v-model="contactDetails"
-                :rules="[v => v.length <= 300 || 'Max 300 characters']"
-                label="How can we reach you? (optional)"
-                hint="For example: email address, Telegram handle, phone number"
-              />
-              <v-select
-                :items=items
-                v-model="subject"
-                label="What's the main reason for your message?"
-              />
-              <v-textarea
-                id="inputMessage"
-                counter
-                v-model="message"
-                :rules="[v => v.length <= 10000 || 'Message must be 10000 characters or less']"
-                auto-grow
-                label="Message"
-              />
-              <div align="right">
-                <v-btn color="primary" @click="send">
-                  SEND MESSAGE
-                </v-btn>
-              </div>
-            </div>
-          </v-col>
-        </v-row>
+      <v-row>
+        <v-col></v-col>
+        <v-col align-self="center">
+          <v-responsive max-width="840px" min-width="320px" >
+              <h1>Get in touch with the team</h1>
+              <p>Thank you for your interest. We really want to hear what you've got to say.</p>
+              <p>Have you got a question? First, check our <a href="https://www.mediawiki.org/wiki/Talk:Wikibase/FAQ" target="_blank">FAQ</a> to see if we've already answered it. We're constantly  updating it with new questions. if you don't find an answer there, feel free to use the contact form below. Thanks!</p>
+              <h3>Want to connect with the Wikibase.cloud community?</h3>
+              <p>You can visit our <a href="https://t.me/joinchat/FgqAnxNQYOeAKmyZTIId9g" target="_blank">Telegram channel</a> or join our <a href="https://lists.wikimedia.org/postorius/lists/wikibase-cloud.lists.wikimedia.org/" target="_blank">mailing list</a>.</p>
+              <v-responsive max-width="512px">
+                <h2 class="mt-10 mb-2">Contact Us</h2>
+                <v-text-field
+                  id="Name"
+                  v-model="name"
+                  :rules="[() => !!name || 'This field is required', v => v.length <= 300 || 'Max 300 characters']"
+                  type="text"
+                  label="Your Name"
+                />
+                <v-text-field
+                  id="contactDetail"
+                  v-model="contactDetails"
+                  :rules="[v => v.length <= 300 || 'Max 300 characters']"
+                  label="How can we reach you? (optional)"
+                  hint="For example: email address, Telegram handle, phone number"
+                />
+                <v-select
+                  :items=items
+                  v-model="subject"
+                  label="What's the main reason for your message?"
+                />
+                <v-textarea
+                  id="inputMessage"
+                  counter
+                  v-model="message"
+                  :rules="[v => v.length <= 10000 || 'Message must be 10000 characters or less']"
+                  auto-grow
+                  label="Message"
+                />
+                <div align="right">
+                  <v-btn color="primary" @click="send">
+                    SEND MESSAGE
+                  </v-btn>
+                </div>
+              </v-responsive>
           <v-snackbar color="success" elevation="24" v-model="successMessage">
             Your message has been sent. Thanks!
             <template v-slot:action>
@@ -69,7 +68,10 @@
               </v-btn>
             </template>
           </v-snackbar>
-      </v-container>
+      </v-responsive>
+        </v-col>
+        <v-col></v-col>
+      </v-row>
     </v-main>
 </template>
 
