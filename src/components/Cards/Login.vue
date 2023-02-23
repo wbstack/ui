c9165639b293962ec2a6f9c8885e128bb2435f01
Deniz Erdogan <deniz.erdogan@wikimedia.de>
2023-02-23 16:44:27 +0100

    Clean up

--- src/components/Cards/Login.vue
+++ src/components/Cards/Login.vue
@@ -78,10 +78,7 @@ export default {
           console.log(err.response)
 
           // If the api gave use details of the error, then use them
-          if (err.response !== undefined &&
-                err.response.data !== undefined &&
-                err.response.data.errors !== undefined
-          ) {
+          if (err.response && err.response.data && err.response.data.errors) {
             if (err.response.data.errors.email) {
               this.error.email = err.response.data.errors.email[0]
             }
