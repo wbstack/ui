b50841cd493516b9d11d22ea203a932993b232ae
Thomas Arrow <thomas.arrow@wikimedia.de>
2023-07-27 17:23:12 +0100

    Adjust bar and button height and button margins

--- src/components/Layout/Navbar.vue
+++ src/components/Layout/Navbar.vue
@@ -1,19 +1,18 @@
 <template>
-  <v-toolbar>
+  <v-toolbar height="64px">
     <a class="pr-4 logo" href="/">
       <img class="logo" src="../../assets/logo.svg">
     </a>
     <v-spacer></v-spacer>
-    <v-toolbar-items>
       <template v-if="isLoggedIn">
         <v-btn class="text-none no-button-pointer-events" text> Hi {{ currentUser.email }}</v-btn>
         <v-btn id="nav-dashboard" text to="/dashboard">Dashboard</v-btn>
         <v-btn id="nav-user" text to="/user">Account</v-btn>
         <v-btn id="nav-logout" text to="/logout">Logout</v-btn>
       </template>
-      <v-menu offset-y nudge-x="20px" transition="scale-transition" v-if="($vuetify.breakpoint.width < 600) && !isLoggedIn">
+        <v-menu internal-activator offset-y transition="scale-transition" v-if="($vuetify.breakpoint.width < 600) && !isLoggedIn">
         <template v-slot:activator="{ on, attrs }">
-          <v-btn class="dots-button" v-bind="attrs" v-on="on">
+          <v-btn class="dots-button mr-4" v-bind="attrs" v-on="on">
             <v-icon>mdi-dots-horizontal</v-icon>
           </v-btn>
         </template>
@@ -27,10 +26,9 @@
         </v-list>
       </v-menu>
       <template v-else>
-        <v-btn id="nav-create-account" text to="/create-account">Sign up</v-btn>
-        <v-btn id="nav-login" color="primary" to="/login">Log in</v-btn>
+        <v-btn id="nav-create-account" large class="mr-4" text to="/create-account">Sign up</v-btn>
+        <v-btn id="nav-login" class="mr-4" large color="primary" to="/login">Log in</v-btn>
       </template>
-    </v-toolbar-items>
   </v-toolbar>
 </template>
 
