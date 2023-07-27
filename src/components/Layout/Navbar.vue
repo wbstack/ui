43d452516c64386d34c16fcd26dfff4251645dc8
Thomas Arrow <thomas.arrow@wikimedia.de>
2023-07-27 10:57:48 +0100

    Use offset-y to make menu dots visible on activation

--- src/components/Layout/Navbar.vue
+++ src/components/Layout/Navbar.vue
@@ -11,7 +11,7 @@
         <v-btn id="nav-user" text to="/user">Account</v-btn>
         <v-btn id="nav-logout" text to="/logout">Logout</v-btn>
       </template>
-      <v-menu transition="scale-transition" v-if="($vuetify.breakpoint.width < 600) && !isLoggedIn">
+      <v-menu offset-y transition="scale-transition" v-if="($vuetify.breakpoint.width < 600) && !isLoggedIn">
         <template v-slot:activator="{ on, attrs }">
           <v-btn class="dots-button" v-bind="attrs" v-on="on">
             <v-icon>mdi-dots-horizontal</v-icon>
