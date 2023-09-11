3b26577d4a35cc2594f6797f3c279106415c1579
Perside Rosalie <perside.rosalie@wikimedia.de>
2023-09-11 10:12:32 +0200

    Make navbar responsive for small screen sizes (#660)
    * Make nav bar responsive for small screen sizes
    
    * change icon from dot-vertical to menu
    
    * fix menu to be dropdown
    
    * Removed unused css class
    
    * remove dots button box-shadow and background-color
    
    * fix: Change breakpoint to 434px
    
    * fix(header): list items in navbar menu do not match spec
    
    * fix: text in dropdown menu should not wrap
    
    * fix: change breakpoints and refactor <v-menu>
    
    * Use offset-y to make menu dots visible on activation
    
    * Use nudge-x to keep menu visible
    
    * Adjust bar and button height and button margins
    
    * fix: Use content-class to move dropdown menu card
    
    * fix: Fix SignUp and LogIn Buttons appear when logged in
    
    * fix: fix spacing and size of signup and login buttons
    
    * fix: change buttons' position and hover shadow
    
    * fix: remove underline on buttons
    
    * fix: remove buttons' elevation and focus state
    
    * fix: fix button
    hovering effect
    
    * fix: fix hover highlight effect
    
    * fix: v-btn expects a boolean for icon
    
    * chore: revert package-lock.json changes
    
    ---------
    
    Co-authored-by: Dat <dat.nguyen@wikimedia.de>
    Co-authored-by: Frederik Ring <frederik.ring@wikimedia.de>
    Co-authored-by: Thomas Arrow <thomas.arrow@wikimedia.de>
    Co-authored-by: Andrew Kostka <andrew.kostka@wikimedia.de>

--- src/components/Layout/Navbar.vue
+++ src/components/Layout/Navbar.vue
@@ -1,21 +1,53 @@
 <template>
-  <v-toolbar>
+  <v-toolbar height="64px">
     <a class="pr-4 logo" href="/">
       <img class="logo" src="../../assets/logo.svg">
     </a>
     <v-spacer></v-spacer>
-    <v-toolbar-items>
       <template v-if="isLoggedIn">
         <v-btn class="text-none no-button-pointer-events" text> Hi {{ currentUser.email }}</v-btn>
         <v-btn id="nav-dashboard" text to="/dashboard">Dashboard</v-btn>
         <v-btn id="nav-user" text to="/user">Account</v-btn>
         <v-btn id="nav-logout" text to="/logout">Logout</v-btn>
       </template>
-      <template v-if="!isLoggedIn">
-        <v-btn id="nav-login" text to="/login">Log in</v-btn>
-        <v-btn id="nav-create-account" ext color="primary" to="/create-account">Sign up</v-btn>
+      <v-menu content-class="menu" internal-activator offset-y transition="scale-transition" v-if="($vuetify.breakpoint.width < 600) && !isLoggedIn">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon class="dots-button mr-4" v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-horizontal</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item class="text-no-wrap" to="/login" style="text-decoration: none;">
+            <v-list-item-title>Log In</v-list-item-title>
+          </v-list-item>
+          <v-list-item class="text-no-wrap" to="/create-account" style="text-decoration: none;">
+            <v-list-item-title>Sign Up</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <template v-if="($vuetify.breakpoint.width >= 600) && !isLoggedIn">
+        <v-btn
+          id="nav-login"
+          class="mr-4"
+          large
+          text
+          style="text-decoration: none;"
+          to="/login"
+        >
+          Log In
+        </v-btn>
+        <v-btn
+          id="nav-create-account"
+          class="mr-4"
+          large
+          elevation="0"
+          color="primary"
+          style="text-decoration: none;"
+          to="/create-account"
+        >
+          Sign Up
+        </v-btn>
       </template>
-    </v-toolbar-items>
   </v-toolbar>
 </template>
 
@@ -48,5 +80,16 @@ text-decoration-line: underline;
 }
 .logo{
   height: 64px;
+}
+.dots-button{
+  box-shadow: none;
+  background-color: transparent !important;
+}
+.menu{
+  margin-top: 2px;
+  margin-left: -5px;
+}
+.theme--light.v-btn--active:before {
+  opacity: 0;
 }
 </style>
