5c584b9bf599b80ce533adfdf0be2dfc2bb1ebac
Andrew Kostka <andrew.kostka@wikimedia.de>
2023-12-08 10:15:55 +0000

    Switch from PNGs to SVGs

--- src/components/Pages/Home/Home.vue
+++ src/components/Pages/Home/Home.vue
@@ -13,26 +13,26 @@
       >
         <DynamicPhotoTextView
           :sideBySide="sideBySide"
-          :image="require('../../../assets/components/pages/home/BlueGraph.png')"
+          :image="require('../../../assets/components/pages/home/BlueGraph.svg')"
           header="What is Wikibase Cloud?"
           content="In a nutshell, Wikibase Cloud is a collaborative space for individuals and groups to contribute, edit, and curate information in a structured way to help you transform your data into meaningful knowledge."
         />
         <DynamicPhotoTextView
           :sideBySide="sideBySide"
-          :image="require('../../../assets/components/pages/home/RedHeads.png')"
+          :image="require('../../../assets/components/pages/home/RedHeads.svg')"
           header="Why Wikibase Cloud?"
           content="Wikibase Cloud makes it easy and quick to host your Wikibase with no need for your own infrastructure. We offer a complete install of Wikibase and its services within a few minutes of signup. Wikibase cloud is GDPR-compliant, rule-free, flexible and free and open source."
         />
         <DynamicPhotoTextView
           :sideBySide="sideBySide"
-          :image="require('../../../assets/components/pages/home/GreenGroup.png')"
+          :image="require('../../../assets/components/pages/home/GreenGroup.svg')"
           header="Who is Wikibase Cloud for?"
           content="Whether you're in the cultural sector, a scientific researcher or the author of another data-rich project, Wikibase Cloud supports the archiving of structured data collections and opens the doors to efficient data management, sharing, and discovering insights in a collaborative environment."
         />
       </DynamicContainerView>
       <DiscoveryView/>
       <PhotoTextView
-        :image="require('../../../assets/components/pages/home/GreenLady.png')"
+        :image="require('../../../assets/components/pages/home/GreenLady.svg')"
         header="Sign up for Open Beta!"
         content="Our Open Beta is ready for your projects and data, running in European data centers with full GDPR compliance and 30 days of guaranteed data backups. Beta means we may not be able to offer 100% availability on the platform or individual services until full release, but we can guarantee your data is safe."
         button="Sign up here for open beta"
