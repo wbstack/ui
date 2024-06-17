f620a4ef3aab12b10aef6f56f9978846622af9d2
Dat <dat.nguyen@wikimedia.de>
2024-06-17 14:44:51 +0200

    fix: shorten url test

--- tests/e2e/specs/app.spec.js
+++ tests/e2e/specs/app.spec.js
@@ -2,54 +2,42 @@ const App = require('../pageobjects/app.page')
 
 describe('Vue.js app', () => {
   it('should render the WBC logo', async () => {
-    App.open()
+    beforeEach(async () => {
+      App.open()
+    })
     expect(App.logo).toExist()
   })
 
   it('should open and render with Login button', async () => {
-    App.open()
+    beforeEach(async () => {
+      App.open()
+    })
     const navLogin = await $('#nav-login')
 
     // Assert that the element is clickable
     expect(await navLogin.isClickable()).toBe(true)
     await navLogin.click()
     // Wait for the URL to change to the signup URL
-    await browser.waitUntil(
-      async () => (await browser.getUrl()).includes('/login'),
-      {
-        timeout: 10000,
-        timeoutMsg: 'expected to be redirected to /login within 5s'
-      }
-    )
-
-    // Assert the new URL
-    const signupUrl = await browser.getUrl()
-    expect(signupUrl).toContain('/login')
+    await expect(browser).toHaveUrlContaining('/login', { wait: 5000 })
   })
 
   it('should open and render with Signup button', async () => {
-    App.open()
+    beforeEach(async () => {
+      App.open()
+    })
     const navCreateAccount = await $('#nav-create-account')
 
     // Assert that the element is clickable
     expect(await navCreateAccount.isClickable()).toBe(true)
     await navCreateAccount.click()
     // Wait for the URL to change to the signup URL
-    await browser.waitUntil(
-      async () => (await browser.getUrl()).includes('/create-account'),
-      {
-        timeout: 10000,
-        timeoutMsg: 'expected to be redirected to /create-account within 5s'
-      }
-    )
-
-    // Assert the new URL
-    const signupUrl = await browser.getUrl()
-    expect(signupUrl).toContain('/create-account')
+    await expect(browser).toHaveUrlContaining('/create-account', { wait: 5000 })
   })
 
   it('should collapse Login and Signup buttons into a dots-button icon when screen width < 600px', async () => {
-    App.open()
+    beforeEach(async () => {
+      App.open()
+    })
     await browser.setWindowSize(599, 800)
     const dotsButton = await $('#dots-button')
     await dotsButton.waitForDisplayed()
@@ -59,7 +47,9 @@ describe('Vue.js app', () => {
   })
 
   it('should have a button linking to Discovery page', async () => {
-    App.open()
+    beforeEach(async () => {
+      App.open()
+    })
     const discoveryButton = await $('#discovery-button')
     await discoveryButton.waitForDisplayed()
 
@@ -68,21 +58,13 @@ describe('Vue.js app', () => {
     // CLick the button
     await discoveryButton.click()
     // Wait for the URL to change to the discovery URL
-    await browser.waitUntil(
-      async () => (await browser.getUrl()).includes('/discovery'),
-      {
-        timeout: 5000,
-        timeoutMsg: 'expected to be redirected to /discovery within 5s'
-      }
-    )
-
-    // Assert the new URL
-    const discoveryUrl = await browser.getUrl()
-    expect(discoveryUrl).toContain('/discovery')
+    await expect(browser).toHaveUrlContaining('/discovery', { wait: 5000 })
   })
 
   it('should prompt users to sign up for open Beta', async () => {
-    App.open()
+    beforeEach(async () => {
+      App.open()
+    })
     const openBetaButton = await $('#open-beta-button')
     await openBetaButton.waitForDisplayed()
 
@@ -91,31 +73,27 @@ describe('Vue.js app', () => {
     // CLick the button
     await openBetaButton.click()
     // Wait for the URL to change to the signup URL
-    await browser.waitUntil(
-      async () => (await browser.getUrl()).includes('/create-account'),
-      {
-        timeout: 10000,
-        timeoutMsg: 'expected to be redirected to /create-account within 5s'
-      }
-    )
-
-    // Assert the new URL
-    const signupUrl = await browser.getUrl()
-    expect(signupUrl).toContain('/create-account')
+    await expect(browser).toHaveUrlContaining('/create-account', { wait: 5000 })
   })
 
   it('should open and render some text to introduce WBCloud', () => {
-    App.open()
+    beforeEach(async () => {
+      App.open()
+    })
     expect(App.wbcloudIntro).toExist()
   })
 
   it('should open and render a footer element', () => {
-    App.open()
+    beforeEach(async () => {
+      App.open()
+    })
     expect(App.footer).toExist()
   })
 
   it('should open and show 3 featured wikis', () => {
-    App.open()
+    beforeEach(async () => {
+      App.open()
+    })
     expect(App.featuredWiki1).toExist()
     expect(App.featuredWiki2).toExist()
     expect(App.featuredWiki3).toExist()
