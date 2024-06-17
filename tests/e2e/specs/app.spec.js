4d6ff4d36158458da5d7df6e61c85eb8423421db
Dat <dat.nguyen@wikimedia.de>
2024-06-17 23:23:21 +0200

    fix: add waiting time for buttons to be rendered

--- tests/e2e/specs/app.spec.js
+++ tests/e2e/specs/app.spec.js
@@ -24,73 +24,67 @@ describe('Vue.js app', () => {
   it('should open and render with Login button', async () => {
     beforeEach(async () => {
       App.open()
-      App.waitForDisplayed({ timeout: 2000 })
     })
-    const navLogin = await $('#nav-login')
 
-    // Assert that the element is clickable
+    const navLogin = await $('#nav-login')
+    await navLogin.waitForDisplayed()
+
     expect(await navLogin.isClickable()).toBe(true)
     await navLogin.click()
-    // Wait for the URL to change to the signup URL
+
     await expect(browser).toHaveUrlContaining('/login', { wait: 5000 })
   })
 
   it('should open and render with Signup button', async () => {
     beforeEach(async () => {
       App.open()
-      App.waitForDisplayed({ timeout: 2000 })
     })
-    const navCreateAccount = await $('#nav-create-account')
 
-    // Assert that the element is clickable
+    const navCreateAccount = await $('#nav-create-account')
+    await navCreateAccount.waitForDisplayed()
+
     expect(await navCreateAccount.isClickable()).toBe(true)
     await navCreateAccount.click()
-    // Wait for the URL to change to the signup URL
+
     await expect(browser).toHaveUrlContaining('/create-account', { wait: 5000 })
   })
 
   it('should collapse Login and Signup buttons into a dots-button icon when screen width < 600px', async () => {
     beforeEach(async () => {
       App.open()
-      App.waitForDisplayed({ timeout: 2000 })
     })
+
     await browser.setWindowSize(599, 800)
     const dotsButton = await $('#dots-button')
     await dotsButton.waitForDisplayed()
 
-    // Assert that the element is displayed
     expect(await dotsButton.isDisplayed()).toBe(true)
   })
 
   it('should have a button linking to Discovery page', async () => {
     beforeEach(async () => {
       App.open()
-      App.waitForDisplayed({ timeout: 2000 })
     })
     const discoveryButton = await $('#discovery-button')
     await discoveryButton.waitForDisplayed()
 
-    // Assert that the element is clickable
     expect(await discoveryButton.isClickable()).toBe(true)
-    // CLick the button
     await discoveryButton.click()
-    // Wait for the URL to change to the discovery URL
+
     await expect(browser).toHaveUrlContaining('/discovery', { wait: 5000 })
   })
 
   it('should prompt users to sign up for open Beta', async () => {
     beforeEach(async () => {
       App.open()
-      App.waitForDisplayed({ timeout: 2000 })
     })
+
     const openBetaButton = await $('#open-beta-button')
     await openBetaButton.waitForDisplayed()
 
-    // Assert that the element is clickable
     expect(await openBetaButton.isClickable()).toBe(true)
-    // CLick the button
     await openBetaButton.click()
-    // Wait for the URL to change to the signup URL
+
     await expect(browser).toHaveUrlContaining('/create-account', { wait: 5000 })
   })
 })
