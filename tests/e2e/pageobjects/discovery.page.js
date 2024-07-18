5b5cd0a04d6cf619b9f417c4cc396f6c689a6c82
Andrew Kostka <andrew.kostka@wikimedia.de>
2024-07-18 15:27:49 +0000

    Add additional tests

--- tests/e2e/pageobjects/discovery.page.js
+++ tests/e2e/pageobjects/discovery.page.js
@@ -1,6 +1,30 @@
 class Discovery {
-  get excludeEmptyToggle () { return $('//label[contains(text(), "exclude empty Wikibases")]') }
-  get emptySeededSite () { return $('//div[contains(text(), "seededSite")]') }
+  get pagination () { return $('.pagination') }
+  get sortDropdown () { return $('.options .v-select') }
+  get sortValue () { return $('.options .v-select__selection') }
+  get excludeEmptyCheckbox () { return $('.options .v-input--checkbox input') }
+  get cards () { return $('.grid.cards') }
+  get firstCard () { return $('.card:first-child .text-h5') }
+  get lastCard () { return $('.card:last-child .text-h5') }
+
+  getCardByWikiName (name) {
+    return $('//div[contains(text(), "' + name + '")]')
+  }
+
+  getCardByPageCount (count) {
+    return $('//div[contains(text(), "No. of pages: ' + count + '")]')
+  }
+
+  async setSortValue (value) {
+    const sortDropdown = await this.sortDropdown
+    await sortDropdown.click()
+
+    const dropdownOption = await $(
+      '//div[contains(@class, "v-list-item__title") and contains(text(), "' + value + '")]'
+    )
+    await dropdownOption.waitForDisplayed({ timeout: 5000 })
+    await dropdownOption.click()
+  }
 
   open (path = '/discovery') {
     browser.url(path)
--- tests/e2e/specs/discovery.spec.js
+++ tests/e2e/specs/discovery.spec.js
@@ -6,13 +6,89 @@ describe('Discovery page', () => {
     await browser.setWindowSize(1360, 973)
   })
 
-  it('should allow filtering out empty instances', async () => {
-    const excludeEmptyToggle = await Discovery.excludeEmptyToggle
-    await excludeEmptyToggle.waitForDisplayed({ timeout: 5000 })
-    await excludeEmptyToggle.click()
+  it('should paginate results', async () => {
+    const page = await Discovery.pagination
+    expect(await page.isDisplayed()).toBe(true)
+  })
 
-    const emptySeededSite = await Discovery.emptySeededSite
-    await emptySeededSite.waitForDisplayed({ timeout: 5000 })
-    expect(await emptySeededSite.isDisplayed()).toBe(true)
+  it('should filter by descending page count by default', async () => {
+    const sortValue = await Discovery.sortValue
+    const cardWithMostPages = await Discovery.getCardByPageCount(49)
+    const cardWithLeastPages = await Discovery.getCardByPageCount(0)
+
+    expect(await sortValue.getText()).toStrictEqual('No. of pages ↓')
+    expect(await cardWithMostPages.isDisplayed()).toBe(true)
+    expect(await cardWithLeastPages.isDisplayed()).toBe(false)
+  })
+
+  describe('should support filtering', () => {
+    const cases = [
+      {
+        description: 'in descending alphabetical order',
+        order: 'Alphabetically ↓',
+        cards: {
+          first: 'seededsite-9',
+          last: 'seededsite-32'
+        }
+      },
+      {
+        description: 'in ascending alphabetical order',
+        order: 'Alphabetically ↑',
+        cards: {
+          first: 'seededsite-10',
+          last: 'seededsite-31'
+        }
+      },
+      {
+        description: 'by descending page count',
+        order: 'No. of pages ↓',
+        cards: {
+          first: 'seededsite-49',
+          last: 'seededsite-26'
+        }
+      }
+    ]
+
+    cases.forEach(async (test) => {
+      await it(test.description, async () => {
+        await Discovery.setSortValue(test.order)
+
+        const cards = await Discovery.cards
+        await cards.waitForDisplayed({ timeout: 5000 })
+        const firstCard = await Discovery.firstCard
+        const lastCard = await Discovery.lastCard
+
+        expect(await firstCard.getText()).toStrictEqual(test.cards.first)
+        expect(await lastCard.getText()).toStrictEqual(test.cards.last)
+      })
+    })
+  })
+
+  it('should filter out empty instances by default', async () => {
+    await Discovery.setSortValue('Alphabetically ↑')
+
+    const excludeEmptyCheckbox = await Discovery.excludeEmptyCheckbox
+    const cards = await Discovery.cards
+    await cards.waitForDisplayed({ timeout: 5000 })
+    const cardWithLeastPages = await Discovery.getCardByPageCount(0)
+
+    expect(await excludeEmptyCheckbox.isSelected()).toBe(true)
+    expect(await cardWithLeastPages.isDisplayed()).toBe(false)
+  })
+
+  it('should support including empty instances', async () => {
+    await Discovery.setSortValue('Alphabetically ↑')
+
+    const excludeEmptyCheckbox = await Discovery.excludeEmptyCheckbox
+    await excludeEmptyCheckbox.waitForExist({ timeout: 5000 })
+    const excludeEmptyCheckboxWrapper = await excludeEmptyCheckbox.parentElement()
+    await excludeEmptyCheckboxWrapper.click()
+
+    const cards = await Discovery.cards
+    await cards.waitForDisplayed({ timeout: 5000 })
+    const cardWithLeastPages = await Discovery.getCardByPageCount(0)
+
+    expect(await excludeEmptyCheckbox.isSelected()).toBe(false)
+    expect(await cardWithLeastPages.isDisplayed()).toBe(true)
   })
 })
