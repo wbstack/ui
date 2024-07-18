e95f399ff437d139624feaf20e1243894e14fe44
Frederik Ring <frederik.ring@wikimedia.de>
2024-07-18 15:26:45 +0200

    feat: supply feedback about import progress by polling

--- src/components/Pages/ManageWiki/Cards/EntityImport.vue
+++ src/components/Pages/ManageWiki/Cards/EntityImport.vue
@@ -7,32 +7,55 @@
         <a href="https://www.wikidata.org" target="_blank">See exact list of entities here.</a>
       </v-card-text>
       <v-card-actions>
-        <v-tooltip top>
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn v-bind="attrs" v-on="on" type="submit">Copy entities</v-btn>
-          </template>
-          <span>You can only import the base set once per Wikibase instance.</span>
-        </v-tooltip>
+        <v-btn :disabled="hasPending || hasSuccess" type="submit">Copy entities</v-btn>
       </v-card-actions>
+      <v-card-text v-if="!hasPending && !hasSuccess && hasFailed">
+        <span class="font-italic">Your entity import failed. Please try again.</span>
+      </v-card-text>
+      <v-card-text v-if="!hasPending && hasSuccess">
+        <span class="font-italic">You can only import the base set once per Wikibase instance.</span>
+      </v-card-text>
+      <v-card-text v-if="hasPending">
+        <span class="font-italic">The wiki currently has an entity import in progress.</span>
+      </v-card-text>
+      <Interval
+        v-if="hasPending"
+        :callback="pollForStatus"
+        :interval="3000"
+      />
     </v-card>
   </v-form>
 </template>
 
 <script>
+import Interval from '@/components/Util/Interval'
+
 export default {
+  components: {
+    Interval
+  },
   name: 'EntityImport',
   props: [
     'wikiId'
   ],
-  data () {
-    return {}
-  },
-  created () {
+  computed: {
+    hasPending () {
+      return this.$store.getters.currentWikiEntityImports.some((i) => i.status === 'pending')
+    },
+    hasSuccess () {
+      return this.$store.getters.currentWikiEntityImports.some((i) => i.status === 'success')
+    },
+    hasFailed () {
+      return this.$store.getters.currentWikiEntityImports.some((i) => i.status === 'failed')
+    }
   },
   methods: {
     doSubmit (evt) {
       evt.preventDefault()
       this.$store.dispatch('triggerEntityImport', this.wikiId)
+    },
+    pollForStatus () {
+      this.$store.dispatch('updateEntityImports', this.wikiId)
     }
   }
 }
--- src/store/wikis.js
+++ src/store/wikis.js
@@ -6,7 +6,7 @@ const getDefaultState = () => {
     wikis: [],
     count: 0,
     limit: 0,
-    entityImports: [],
+    currentWikiEntityImports: [],
     currentWikiSettings: null
   }
 }
@@ -34,7 +34,7 @@ const getters = {
   wikis: state => state.wikis,
   wikiCount: state => state.count,
   wikiLimit: state => state.limit,
-  hasEntityImport: state => state.entityImports.some((i) => i.status === 'success' || i.status === 'pending'),
+  currentWikiEntityImports: state => state.currentWikiEntityImports,
   hasLoaded: state => state.wikis.status !== ''
 }
 
@@ -53,12 +53,6 @@ const mutations = {
   },
   wikis_error (state) {
     state.status = 'error'
-  },
-  entityImports_success (state, response) {
-    state.entityImports = response
-  },
-  entityImports_error (state) {
-    state.entityImports = []
   },
   set_current_wiki_settings (state, details) {
     const wgDefaultSkinSetting = details.public_settings.find(setting => setting.name === 'wgDefaultSkin')
@@ -150,15 +144,17 @@ const mutations = {
   },
   set_questy_captcha_questions (state, value) {
     state.currentWikiSettings.captchaQuestions = value
+  },
+  set_current_wiki_entityImports (state, response) {
+    state.currentWikiEntityImports = response
   }
 }
 
 const actions = {
   initializeSettings ({ commit }, wikiId) {
     commit('clear_current_wiki_settings')
-    api.getEntityImports({ wiki: wikiId })
-      .then(res => commit('entityImports_success', res))
-      .catch(err => commit('entityImports_error', err))
+    api.getEntityImports({ wikiId })
+      .then(res => commit('set_current_wiki_entityImports', res))
     api.wikiDetails({ wiki: wikiId })
       .then(details => commit('set_current_wiki_settings', details))
   },
@@ -191,8 +187,16 @@ const actions = {
   updateSetting ({ commit }, payload) {
     return api.updateSetting(payload.setting, payload)
   },
-  triggerEntityImport (_, wikiId) {
+  triggerEntityImport ({ commit }, wikiId) {
     return api.importEntities({ wikiId })
+      .then(() => {
+        return api.getEntityImports({ wikiId })
+      })
+      .then(res => commit('set_current_wiki_entityImports', res))
+  },
+  updateEntityImports ({ commit }, wikiId) {
+    return api.getEntityImports({ wikiId })
+      .then(res => commit('set_current_wiki_entityImports', res))
   },
   setItemMapping ({ commit }, mapping) {
     commit('set_item_mapping', mapping)
