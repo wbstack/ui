b00f1894c9417cb4154aa6d0abf3c00118c71155
Perside Rosalie <perside.rosalie@wikimedia.de>
2024-08-08 15:12:14 +0200

    Revert "Revert "T347865: Ask for feedback before deleting a wikibase instance…" (#833)
    This reverts commit 78b8580263a09238325828100eb8b5b417ef7548.

--- src/components/Pages/ManageWiki/Cards/Delete.vue
+++ src/components/Pages/ManageWiki/Cards/Delete.vue
@@ -1,43 +1,27 @@
 <template>
-  <v-card color="#fad1d0">
-    <v-card-title>Delete Site</v-card-title>
-    <v-card-text>
-      Selecting Confirm Deletion will permanently delete the selected Wikibase instance.<br>
-      All data associated with this instance will be purged permanently in 30 days.<br>
-      <br>
-      The domain will <strong>not</strong> be available for reuse.
-    </v-card-text>
-    <v-card-actions>
-      <v-btn v-if="!expandDelete" @click="expandDelete = true" color="red">Delete site</v-btn>
-      <v-btn v-if="expandDelete" @click="expandDelete = false" color="blue">Cancel deletion</v-btn>
-      <v-btn v-if="expandDelete" @click="doDelete" color="red">Confirm deletion</v-btn>
-    </v-card-actions>
+  <v-card class="pa-4" color="#fad1d0">
+    <p class="text-h5 ma-0">Delete Wikibase Instance</p>
+    <p class="text-sm-body-2">
+      Selecting Confirm Deletion will permanently delete the selected Wikibase instance. All data associated <br/>
+      with this instance will be purged permanently in 30 days. The domain will <strong>not</strong> be available for reuse.
+    </p>
+    <v-btn @click="expandDelete = true" color="red" class="text-sm-button white--text">Delete site</v-btn>
+    <ConfirmDelete v-if="expandDelete" :wiki-id=this.wikiId @close="expandDelete=false"/>
   </v-card>
 </template>
 
 <script>
+import ConfirmDelete from '@/components/Pages/ManageWiki/Cards/ConfirmDelete.vue'
+
 export default {
   name: 'Delete',
+  components: { ConfirmDelete },
   props: [
     'wikiId'
   ],
   data () {
     return {
       expandDelete: false
-    }
-  },
-  methods: {
-    doDelete () {
-      const wiki = this.wikiId
-
-      this.$store
-        .dispatch('deleteWiki', { wiki })
-        .then(() => this.$router.push('/dashboard'))
-        .catch(err => {
-          console.log(err.response)
-          alert('Something went wrong.')
-          this.$router.push('/dashboard')
-        })
     }
   }
 }
