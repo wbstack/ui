840d55d3c87d160ea6596d1936d53f168f2e4bfd
Frederik Ring <frederik.ring@gmail.com>
2024-08-14 12:54:04 +0200

    feat: display which entities will be imported (#849)
    * feat: display which entities will be imported
    
    * fix: update copy
    
    * chore: remove info button from details card
    
    * fix: lists refer to wrong headlines

--- src/components/Pages/ManageWiki/Cards/Details.vue
+++ src/components/Pages/ManageWiki/Cards/Details.vue
@@ -2,13 +2,6 @@
   <v-card>
     <v-card-title>
       Details
-      <v-spacer></v-spacer>
-      <v-tooltip right>
-        <template v-slot:activator="{ on }">
-          <v-icon v-on="on">mdi-information-outline</v-icon>
-        </template>
-        <span>During Alpha if you would like to change these details please contact us.</span>
-      </v-tooltip>
     </v-card-title>
     <v-card-text>
       <strong>Please check your emails for your log in details.</strong><br/>
