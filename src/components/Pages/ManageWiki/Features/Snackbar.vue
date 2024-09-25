a6a9fda1da0340a614f96d9051ce90171164fd0e
dena <dena@wikimedia.de>
2024-09-25 08:51:05 +0200

    add Snackbar component

---
+++ src/components/Pages/ManageWiki/Features/Snackbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-snackbar :color="status" elevation="24" v-model="visible">
+    {{ text }}
+    <template v-slot:action>
+      <v-btn
+        text
+        variant="text"
+        @click="hide"
+      >
+      Close
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      visible: false,
+      text: 'Hello, this is a snackbar message!',
+      status: 'success'
+    };
+  },
+  methods: {
+    show (status, message) {
+      this.status = status
+      this.text = message
+      this.visible = true
+
+      console.log(message)
+      console.log(status)
+    },
+
+    hide () {
+      this.visible = false;
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
