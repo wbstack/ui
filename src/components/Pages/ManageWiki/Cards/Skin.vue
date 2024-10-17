2d09c5a348f8785ff36e6ab14a98bdd73413fe53
dena <91744937+deer-wmde@users.noreply.github.com>
2024-10-17 14:10:30 +0200

    refactor skin selection (#894)
    * refactor skin Card
    * use Message component in QuestyCaptcha

--- src/components/Pages/ManageWiki/Cards/Skin.vue
+++ src/components/Pages/ManageWiki/Cards/Skin.vue
@@ -3,15 +3,15 @@
     <v-card-title>Set Skin</v-card-title>
     <v-card-text>
       <v-select
-        :items="items"
+        :items="skins"
         label="Skin"
         placeholder="Pick a Skin"
         hint="The default skin is Vector."
         persistent-hint
         prepend-icon="mdi-web"
-        v-model="skin"
         :disabled="inFlight"
         :error-messages="error"
+        v-model="skinId"
       ></v-select>
     </v-card-text>
     <v-card-actions>
@@ -22,45 +22,62 @@
         <span>It may take up to 10 seconds for changes to be reflected on your wiki</span>
       </v-tooltip>
     </v-card-actions>
+    <Message ref="message" />
   </v-card>
 </template>
 
 <script>
+import Message from '../Features/Message.vue'
+
 export default {
   name: 'Skin',
+  components: {
+    Message
+  },
   props: [
     'wikiId'
   ],
   data () {
     return {
-      items: [
-        'Vector',
-        'Modern',
-        'Timeless'
+      skins: [
+        {
+          value: 'vector',
+          text: 'Vector'
+        },
+        {
+          value: 'modern',
+          text: 'Modern'
+        },
+        {
+          value: 'timeless',
+          text: 'Timeless'
+        }
       ],
-      skin: '',
-      inFlight: false,
-      error: ''
+      skinId: '',
+      message: false
     }
   },
   created () {
-    const skin = this.$store.state.wikis.currentWikiSettings.wgDefaultSkin
-    this.skin = skin.charAt(0).toUpperCase() + skin.slice(1)
+    this.skinId = this.$store.state.wikis.currentWikiSettings.wgDefaultSkin
+  },
+  computed: {
+    skin () {
+      return this.skins.find(skin => skin.value === this.skinId)
+    }
   },
   methods: {
     doSetSkin () {
       const wiki = this.wikiId
-      // API needs the skin ID which is lower case..
-      const value = this.skin.toLowerCase()
+      const value = this.skin.value
 
       this.$store
         .dispatch('updateSkin', { wiki, value })
         .then(() => {
-          alert('Update success!')
+          this.$refs.message.show('success', `Your default skin has been updated to ${this.skin.text}.`)
         })
         .catch(err => {
-          console.log(err.response)
-          alert('Something went wrong.')
+          console.error(err.response)
+          this.$refs.message.show('error', 'Something went wrong while updating your default skin. Please try again.')
         })
     }
   }
