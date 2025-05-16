7900c71bfcbc31b7a88fce6d3f97010d042c8c1c
dena <91744937+deer-wmde@users.noreply.github.com>
2025-05-16 15:21:19 +0200

    Add profile tab to wiki dashboard (#969)
    * add profile tab
    
    * add Profile component
    
    * Add profile tab to wiki dashboard
    
    * Fix linting errors
    
    ---------
    
    Co-authored-by: Andrew Kostka <andrew.kostka@wikimedia.de>

---
+++ src/components/Pages/ManageWiki/Tabs/ManageProfile.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-main>
+    <v-container>
+      <v-row>
+        <v-col class="card-column">
+          <Details :wikiId="this.wikiId"/>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
+</template>
+
+<script>
+import Details from '~/components/Pages/ManageWiki/Cards/Details'
+
+export default {
+  name: 'ManageWiki',
+  components: {
+    Details
+  },
+  computed: {
+    wikiId: function () {
+      return this.$route.params.id
+    }
+  }
+}
+</script>
+
+<style scoped>
+.v-main >>> .col {
+  max-width: 656px;
+  width: 100vw;
+}
+</style>
