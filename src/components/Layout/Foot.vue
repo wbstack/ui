3bc4e50da278362a7b9fc2bd15de433b397ca597
Dat <dat.nguyen@wikimedia.de>
2025-08-03 23:28:23 +0200

    Switch to static CSS breakpoint instead of JS

--- src/components/Layout/Foot.vue
+++ src/components/Layout/Foot.vue
@@ -1,14 +1,7 @@
 <template>
   <v-footer data-test-id="footer" class="footer">
-    <v-row
-      :class="isNarrow ? 'small-row-margin stack-vertical' : 'row-margin'"
-      no-gutters
-    >
-      <v-col
-        :class="isNarrow ? 'small-col-margin' : 'col-margin'"
-        ref="col1"
-        cols="auto"
-      >
+    <v-row class="row-margin" no-gutters>
+      <v-col class="col-margin" ref="col1" cols="auto">
         <nav aria-label="Wikibase Cloud links">
           <h3 class="footer-list-title">Wikibase Cloud</h3>
           <ul class="footer-list">
@@ -38,33 +31,7 @@
 
 <script>
 export default {
-  name: 'Footer',
-  data () {
-    return {
-      isNarrow: false,
-      initialThreshold: 0
-    }
-  },
-  mounted () {
-    this.$nextTick(() => {
-      const col1 = this.$refs.col1?.$el || this.$refs.col1
-      const col2 = this.$refs.col2?.$el || this.$refs.col2
-      const col1Width = col1 ? col1.offsetWidth : 0
-      const col2Width = col2 ? col2.offsetWidth : 0
-      const threshold = col1Width + col2Width + 176
-      this.initialThreshold = threshold
-      this.updateIsNarrow()
-      window.addEventListener('resize', this.updateIsNarrow)
-    })
-  },
-  beforeDestroy () {
-    window.removeEventListener('resize', this.updateIsNarrow)
-  },
-  methods: {
-    updateIsNarrow () {
-      this.isNarrow = window.innerWidth <= this.initialThreshold
-    }
-  }
+  name: 'Footer'
 }
 </script>
 
@@ -103,20 +70,21 @@ export default {
 
 .row-margin {
   margin: 48px !important;
-}
-.small-row-margin {
-  margin: 24px !important;
+  display: flex;
 }
 
 .col-margin {
   margin-right: 80px !important;
 }
-.small-col-margin {
-  margin-bottom: 40px !important;
-  margin-right: 0 !important;
-}
 
-.stack-vertical {
-  flex-direction: column !important;
+@media (max-width: 480px) {
+  .row-margin {
+    margin: 24px !important;
+    flex-direction: column !important;
+  }
+  .col-margin {
+    margin-right: 0 !important;
+    margin-bottom: 40px !important;
+  }
 }
 </style>
