00d26fb30ae692f556c20be047f6c53fa2a58aef
Ollie Hyde <oliver.hyde@wikimedia.de>
2026-03-20 17:08:01 +0000

    Minor code refactor for esaier parsing

--- src/components/Cards/KnowledgeEquityCreateWikiWizardStep.vue
+++ src/components/Cards/KnowledgeEquityCreateWikiWizardStep.vue
@@ -43,7 +43,8 @@
           type="warning"
           icon="mdi-alert-outline"
         >
-          Please avoid sharing any personal or sensitive information. This information will be visible to WMDE employees and external members of the <a href="https://www.mediawiki.org/wiki/Wikibase/Wikibase.cloud/Hosting_policy_draft_v0.1#4.7_Review_Committee" target="_blank">review committee</a>.
+          Please avoid sharing any personal or sensitive information. This information will be visible to WMDE employees and external members of the
+          <a href="https://www.mediawiki.org/wiki/Wikibase/Wikibase.cloud/Hosting_policy_draft_v0.1#4.7_Review_Committee" target="_blank">review committee</a>.
         </v-alert>
       </v-form>
     </v-card-text>
