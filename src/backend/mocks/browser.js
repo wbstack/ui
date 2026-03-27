140577e95c95f90e279b5bd1086a16ddf43821d4
Thomas Arrow <thomas.arrow@wikimedia.de>
2026-03-27 13:07:13 +0000

    Try again to fix MSW
    Collaborative attempt in Amsterdam
    
    Bug: T407364

--- src/backend/mocks/browser.js
+++ src/backend/mocks/browser.js
@@ -1,4 +1,4 @@
-import { setupWorker } from 'msw'
-import { handlers } from './default_handlers'
+import { setupWorker } from 'msw/browser'
+import { handlers } from './default_handlers_new'
 
 export const worker = setupWorker(...handlers)
