01a50ab2ed3be3a2c698abf2784ae29633a28180
Ollie Hyde <oliver.hyde@wikimedia.de>
2026-04-09 10:55:47 +0100

    Add `scoped` attribute to `<style>` tags in Single-File Components

--- src/components/Pages/Privacy/Privacy.vue
+++ src/components/Pages/Privacy/Privacy.vue
@@ -777,7 +777,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 h5,
 h4 {
   margin-bottom: 16px;
--- src/components/Pages/TermsOfUse.vue
+++ src/components/Pages/TermsOfUse.vue
@@ -1294,7 +1294,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 h5 {
   margin-bottom: 16px;
 }
