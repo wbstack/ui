ddf9afcd1766ec18294347f059680c5343d7e19f
dena <dena@wikimedia.de>
2026-06-18 09:54:28 +0200

    delete obsolete NavgationLinks.js

--- src/components/Pages/TermsOfUse/NavigationLinks.js
+++
@@ -1,6 +0,0 @@
-const termsOfUseLinks = [
-  { title: 'Upcoming Version', route: 'TermsOfUseUpcoming' },
-  { title: 'April 11th 2022 (current)', route: 'TermsOfUse' },
-]
-
-export default termsOfUseLinks
