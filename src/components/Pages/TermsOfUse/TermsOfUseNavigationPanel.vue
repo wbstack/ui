942de570a5b4fa273353ab0171cf85e421384a8b
dena <dena@wikimedia.de>
2026-07-28 12:31:42 +0200

    fix routing for upcoming policies without a date

--- src/components/Pages/TermsOfUse/TermsOfUseNavigationPanel.vue
+++ src/components/Pages/TermsOfUse/TermsOfUseNavigationPanel.vue
@@ -1,3 +1,5 @@
+<!-- TODO do we wanna move this into TermsOfUseRenderer.vue ? -->
+<!-- TODO make sure any upcoming policy links always go to /upcoming and not /activeFrom -->
 <template>
   <PolicyNavigationPanel :title="title" :links="links" :currentLink="currentLink" />
 </template>
