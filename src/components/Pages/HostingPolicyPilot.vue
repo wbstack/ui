44e80c8bfb116e6e3a04f2c3e75ba70afdb6165a
Perside Rosalie <perside.rosalie@wikimedia.de>
2026-08-03 14:54:30 +0200

    Update src/components/Pages/HostingPolicyPilot.vue
    Co-authored-by: dena <91744937+deer-wmde@users.noreply.github.com>

--- src/components/Pages/HostingPolicyPilot.vue
+++ src/components/Pages/HostingPolicyPilot.vue
@@ -63,7 +63,7 @@
           <p>These conditions must be met for an instance to be eligible for hosting.</p>
           <ul>
             <li> The manager provided sufficient information about their use case for the Review Committee to evaluate the intended use.</li>
-            <li>The instance's observed use matches the description provided information.</li>
+            <li>The instance's observed use matches the provided information.</li>
             <li>The main page or the <code>Project:About</code> has sufficient information describing the intended use of the instance for potential visitors of the instance, or has links to such information.</li>
             <li>The licensing statement is visible on the <code>Project:Copyrights</code> page.</li>
             <li>The structured data is in the public domain or released under <a href="https://www.mediawiki.org/wiki/Wikibase/Wikibase.cloud/Why_CC0" CC-0 />.</li>
