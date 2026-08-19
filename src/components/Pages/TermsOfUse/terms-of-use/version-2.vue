cbe942d5aad8b86fa6d11af5d5353a45f6c2c777
Perside Rosalie <perside.rosalie@wikimedia.de>
2026-08-19 09:52:03 +0200

    Link to the correct ToU previous version

--- src/components/Pages/TermsOfUse/terms-of-use/version-2.vue
+++ src/components/Pages/TermsOfUse/terms-of-use/version-2.vue
@@ -8,7 +8,7 @@
     <v-alert text color="info">
       <div class="light-blue--text text--darken-4">
         <div class="text-h6 mb-3">What changed from the
-          <router-link class="light-blue--text text--darken-4" to="/terms-of-use">
+          <router-link class="light-blue--text text--darken-4" to="/terms-of-use/2022-01-01">
             previous version</router-link>
         </div>
         <p>The Terms of Use were revised to ensure compliance with the European Union’s
