db9295f724371a758c1975b1fc52135774e32784
Ollie Hyde <oliver.hyde@wikimedia.de>
2026-08-26 11:06:46 +0100

    Make same changes to HostingPolicyRenderer in pairing with Tarrow

--- src/components/Pages/HostingPolicy/HostingPolicyRenderer.vue
+++ src/components/Pages/HostingPolicy/HostingPolicyRenderer.vue
@@ -44,14 +44,6 @@ export default {
     PolicyNavigationPanel,
   },
   computed: {
-    isUpcomingPolicy: function () {
-      return this.policyActiveFrom && this.policyActiveFrom === this.upcomingPolicyActiveFrom
-    },
-    isOutdatedPolicy: function () {
-      return this.policyActiveFrom &&
-        this.policyActiveFrom !== this.upcomingPolicyActiveFrom &&
-        this.policyActiveFrom !== this.currentPolicyActiveFrom
-    },
     formattedActiveFrom: function () {
       if (!this.policyActiveFrom) return null
       return new Date(this.policyActiveFrom).toLocaleDateString('en-GB', { year: 'numeric', month: 'long', day: 'numeric', timeZone: 'UTC' })
@@ -62,8 +54,8 @@ export default {
       policyType: 'hosting-policy',
       policyContentComponent: undefined,
       policyActiveFrom: undefined,
-      upcomingPolicyActiveFrom: undefined,
-      currentPolicyActiveFrom: undefined,
+      isUpcomingPolicy: false,
+      isOutdatedPolicy: false,
       error: undefined,
     }
   },
@@ -71,30 +63,34 @@ export default {
     async renderPage () {
       this.policyContentComponent = undefined
       this.policyActiveFrom = undefined
+      this.isUpcomingPolicy = false
+      this.isOutdatedPolicy = false
       this.error = undefined
 
       try {
         const policyType = this.policyType // TODO for a generalized component, read this from component property
         const activeFromRouteParam = this.$route.params.activeFrom
 
-        let upcomingPolicy
-        let currentPolicy
+        let upcomingPolicyResponse
+        let upcomingPolicyActiveFrom
+        let currentPolicyResponse
+        let currentPolicyActiveFrom
         let policyContentVueFile
 
         try {
-          upcomingPolicy = await this.$api.getUpcomingPolicyByType({ policyType })
-          this.upcomingPolicyActiveFrom = upcomingPolicy.metadata.active_from
+          upcomingPolicyResponse = await this.$api.getUpcomingPolicyByType({ policyType })
+          upcomingPolicyActiveFrom = upcomingPolicyResponse.metadata.active_from
         } catch (exception) {
           if (exception instanceof AxiosError && exception.status === 404) {
-            upcomingPolicy = undefined
+            upcomingPolicyResponse = undefined
           } else {
             throw exception
           }
         }
 
         try {
-          currentPolicy = await this.$api.getCurrentPolicyByType({ policyType })
-          this.currentPolicyActiveFrom = currentPolicy.metadata.active_from
+          currentPolicyResponse = await this.$api.getCurrentPolicyByType({ policyType })
+          currentPolicyActiveFrom = currentPolicyResponse.metadata.active_from
         } catch (exception) {
           if (exception instanceof AxiosError && exception.status === 404) {
             // Special case to redirect users to the pilot policy if there is no current policy
@@ -108,28 +104,25 @@ export default {
           }
         }
 
-        // The route can be one of:
-        //   /hosting-policy/upcoming      - upcoming version
-        //   /hosting-policy/:activeFrom   - upcoming version
-        //   /hosting-policy               - current version
-        //   /hosting-policy/:activeFrom   - current version
-        //   /hosting-policy/:activeFrom   - previous version
+        // The routes for an upcoming version can be /hosting-policy/upcoming or /hosting-policy/:activeFrom
         const isUpcomingRoute = this.$route.path === '/hosting-policy/upcoming'
-        const isUpcomingActiveFrom = activeFromRouteParam && activeFromRouteParam === this.upcomingPolicyActiveFrom
+        const isUpcomingActiveFrom = activeFromRouteParam && activeFromRouteParam === upcomingPolicyActiveFrom
+        // The routes for the currently active version can be /hosting-policy or /hosting-policy/:activeFrom
         const isCurrentRoute = this.$route.path === '/hosting-policy'
-        const isCurrentActiveFrom = activeFromRouteParam && activeFromRouteParam === this.currentPolicyActiveFrom
-
+        const isCurrentActiveFrom = activeFromRouteParam && activeFromRouteParam === currentPolicyActiveFrom
         // Determine which policy to render based on the route and the activeFrom parameter
         if (isUpcomingRoute || isUpcomingActiveFrom) {
-          policyContentVueFile = upcomingPolicy.metadata.content_vue_file
-          this.policyActiveFrom = upcomingPolicy.metadata.active_from
+          this.isUpcomingPolicy = true
+          policyContentVueFile = upcomingPolicyResponse.metadata.content_vue_file
+          this.policyActiveFrom = upcomingPolicyResponse.metadata.active_from
         } else if (isCurrentRoute || isCurrentActiveFrom) {
-          policyContentVueFile = currentPolicy.metadata.content_vue_file
-          this.policyActiveFrom = currentPolicy.metadata.active_from
+          policyContentVueFile = currentPolicyResponse.metadata.content_vue_file
+          this.policyActiveFrom = currentPolicyResponse.metadata.active_from
         } else {
-          const previousPolicy = await this.$api.getPolicyByDate({ policyType, activeFrom: activeFromRouteParam })
-          policyContentVueFile = previousPolicy.metadata.content_vue_file
-          this.policyActiveFrom = previousPolicy.metadata.active_from
+          const outdatedPolicy = await this.$api.getPolicyByDate({ policyType, activeFrom: activeFromRouteParam })
+          this.isOutdatedPolicy = true
+          policyContentVueFile = outdatedPolicy.metadata.content_vue_file
+          this.policyActiveFrom = outdatedPolicy.metadata.active_from
         }
 
         const policyContentComponent = () => ({ component: import(`./${policyContentVueFile}`) })
@@ -149,9 +142,7 @@ export default {
     $route: {
       // Run the handler immediately on component creation
       immediate: true,
-      handler (to, from) {
-        this.renderPage()
-      },
+      handler: 'renderPage',
     },
   },
 }
