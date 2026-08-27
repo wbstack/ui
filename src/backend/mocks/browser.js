03e2865f7146abfec543c358e1356e1aeebe4498
dena <dena@wikimedia.de>
2026-08-27 16:11:31 +0200

    wip: start mock service without handlers

--- src/backend/mocks/browser.js
+++ src/backend/mocks/browser.js
@@ -1,4 +1,5 @@
-import { setupWorker } from 'msw'
-import { handlers } from './default_handlers'
+import { setupWorker } from 'msw/browser'
+//import { handlers } from './default_handlers'
 
-export const worker = setupWorker(...handlers)
+//export const worker = setupWorker(...handlers)
+export const worker = setupWorker()
