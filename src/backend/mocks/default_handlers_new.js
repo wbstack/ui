9f3bf5a12e81358db8056ce3db4b633eb899f4fb
Thomas Arrow <thomas.arrow@wikimedia.de>
2026-08-28 10:16:55 +0100

    fix linting

--- src/backend/mocks/default_handlers_new.js
+++ src/backend/mocks/default_handlers_new.js
@@ -11,28 +11,28 @@ function makeUser (email = 'test@local') {
 }
 
 export const handlers = [
-    http.get('/api/auth/login', async ({ request, cookies }) => {
-        const { authToken } = cookies
-        if (authToken !== 'token_value') {
-            return new Response('Unauthorized', {
-                status: 401,
-            })
-        }
-        //   todo this might be broken below here
-        console.log('duck')
-        // const body = await request.json()
-        console.log('duck2')
-        const user = makeUser('thomas.arrow@wikimediasdfa.de')
-        return HttpResponse.json({user})
-    }),
-    http.post('/api/auth/login', async ({ request }) => {
-        const body = await request.json()
-        console.log('fooooooo')
-        console.log(body)
-        const user = makeUser(body.email)
-        return HttpResponse.json({
-            user,
-        }, { headers: { 'set-cookie': 'authToken=token_value', } }
-        )
-    }),
+  http.get('/api/auth/login', async ({ request, cookies }) => {
+    const { authToken } = cookies
+    if (authToken !== 'token_value') {
+      return new Response('Unauthorized', {
+        status: 401,
+      })
+    }
+    //   todo this might be broken below here
+    console.log('duck')
+    // const body = await request.json()
+    console.log('duck2')
+    const user = makeUser('thomas.arrow@wikimediasdfa.de')
+    return HttpResponse.json({ user })
+  }),
+  http.post('/api/auth/login', async ({ request }) => {
+    const body = await request.json()
+    console.log('fooooooo')
+    console.log(body)
+    const user = makeUser(body.email)
+    return HttpResponse.json({
+      user,
+    }, { headers: { 'set-cookie': 'authToken=token_value' } },
+    )
+  }),
 ]
