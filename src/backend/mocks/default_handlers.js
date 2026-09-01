de51e277cc7f63fe9eca9f704786d9b64b122e3c
dena <dena@wikimedia.de>
2026-09-01 17:31:59 +0200

    migrate remaining old handlers

--- src/backend/mocks/default_handlers.js
+++ src/backend/mocks/default_handlers.js
@@ -4,7 +4,10 @@ import { http } from 'msw'
 // see old.default_handlers.js for reference
 // this note can get removed after migration
 
-const myWikis = JSON.parse(localStorage.getItem('msw-myWikis')) || []
+let myWikis = JSON.parse(localStorage.getItem('msw-myWikis')) || []
+let lastWikiId = (myWikis.length && myWikis[myWikis.length - 1].id) || 0
+const user = makeUser()
+let getEntityImportCalledTimes = 0
 
 function makeUser (email = 'test@local') {
   return {
@@ -13,6 +16,121 @@ function makeUser (email = 'test@local') {
     verified: true,
     created_at: '2020-01-01',
     updated_at: '2020-01-01',
+  }
+}
+
+const makeNewWiki = ({ domain, sitename }) => {
+  const newWiki = {
+    id: ++lastWikiId,
+    domain,
+    sitename,
+    deleted_at: null,
+    created_at: '2020-01-01',
+    updated_at: '2020-01-01',
+    pivot: {
+      user_id: user.id,
+      wiki_id: lastWikiId,
+    },
+    wiki_managers: [{
+      email: user.email,
+      pivot: {
+        user_id: user.id,
+        wiki_id: lastWikiId,
+      },
+    }],
+    wiki_db_version: {
+      id: 101,
+      wiki_id: lastWikiId,
+      version: 'mw1.33-wbs1',
+    },
+    public_settings: [],
+  }
+
+  myWikis.push(newWiki)
+  localStorage.setItem('msw-myWikis', JSON.stringify(myWikis))
+
+  return newWiki
+}
+
+const removeWiki = wikiIndex => {
+  myWikis = myWikis.splice(wikiIndex, 1)
+  localStorage.setItem('msw-myWikis', JSON.stringify(myWikis))
+}
+
+const wikiDiscovery = (referrer, params) => {
+  const pseudorandom = {
+    seed: 1,
+    next: function () {
+      const x = Math.sin(this.seed++) * 10000
+      return x - Math.floor(x)
+    },
+  }
+
+  const names = [
+    'Wikibase Name',
+    'A Very Long Wikibase Name',
+  ]
+
+  let wikis = [...Array(75).keys()].map((id) => {
+    const wiki = {
+      id,
+      domain: id + '-wikibase.wbaas.localhost',
+      sitename: id + ' - ' + names[id % names.length],
+      wiki_site_stats: null,
+      logo_url: null,
+    }
+
+    if (pseudorandom.next() >= 0.1) {
+      wiki.wiki_site_stats = {
+        pages: Math.ceil(pseudorandom.next() * 250),
+      }
+    }
+
+    if (pseudorandom.next() >= 0.5) {
+      wiki.logo_url = new URL(referrer).origin + '/favicon.ico'
+    }
+    return wiki
+  })
+
+  if (parseInt(params.get('is_active'))) {
+    wikis = wikis.filter((wiki) => {
+      const stats = wiki.wiki_site_stats
+      return stats && stats.pages > 1
+    })
+  }
+
+  if (params.get('sort') === 'sitename') {
+    wikis = wikis.sort((a, b) => {
+      let sort = a.sitename.localeCompare(b.sitename, 'en', { numeric: true })
+      if (params.get('direction') === 'desc') {
+        sort *= -1
+      }
+      return sort
+    })
+  }
+
+  if (params.get('sort') === 'pages') {
+    wikis = wikis.sort((a, b) => {
+      const aPages = a.wiki_site_stats ? a.wiki_site_stats.pages : 0
+      const bPages = b.wiki_site_stats ? b.wiki_site_stats.pages : 0
+      if (params.get('direction') === 'desc') {
+        return bPages - aPages
+      }
+      return aPages - bPages
+    })
+  }
+
+  const currentPage = parseInt(params.get('page'))
+  const resultsPerPage = parseInt(params.get('per_page'))
+  const start = (currentPage - 1) * resultsPerPage
+  const end = start + resultsPerPage
+
+  return {
+    data: wikis.slice(start, end),
+    meta: {
+      last_page: Math.ceil(wikis.length / resultsPerPage),
+      total: wikis.length,
+    },
   }
 }
 
@@ -75,6 +193,67 @@ export const handlers = [
   http.post('/api/wiki/mine', () => {
     const data = { wikis: myWikis, count: myWikis.length, limit: false }
     return Response.json(data)
+  }),
+
+  http.post('/api/wiki/entityImport', () => {
+    const data = { status: 'pending', payload: {}, started_at: new Date().toJSON() };
+    return Response.json(data)
+  }),
+
+  http.get('/api/wiki/entityImport', () => {
+    getEntityImportCalledTimes++
+    switch (getEntityImportCalledTimes) {
+      case 1:
+        return Response.json([])
+      case 2:
+        return Response.json([{ status: 'pending' }])
+      default:
+        return Response.json([{status: 'success'}])
+    }
+  }),
+
+  http.post('/api/wiki/create', async ({ request }) => {
+    const body = await request.json()
+    const data = makeNewWiki(body)
+    return Response.json(data)
+  }),
+
+  http.post('/api/wiki/delete', async ({ request }) => {
+    const wikiId = await request.json().wiki
+    const wikiIndex = myWikis.findIndex(w => w.id === Number(wikiId))
+
+    if (wikiIndex < 0) {
+      return new Response(null, {status: 404})
+    }
+
+    removeWiki(wikiIndex)
+    return new Response('Success')
+  }),
+
+  http.post('/api/wiki/logo/update', () => {
+    return new Response('Success')
+  }),
+
+  http.post(/\/api\/wiki\/setting\/.*?\/update$/, () => {
+    return new Response('Success')
+  }),
+
+  http.post('/api/wiki/details', async ({ request }) => {
+    const wikiId = await request.json().wiki
+    const wikiDetails = myWikis.find(w => w.id === Number(wikiId))
+
+    if (!wikiDetails) {
+      return new Response(null, {status: 404})
+    }
+    
+    return Response.json(wikiDetails)
+  }),
+
+  http.get('/api/wiki', async ({ request }) => {
+    const referrer = await request.referrer
+    const url = new URL(await request.url)
+
+    return Response.json(wikiDiscovery(referrer, url.searchParams))
   }),
 
   http.get('/api/v1/policies/missing', () => {
