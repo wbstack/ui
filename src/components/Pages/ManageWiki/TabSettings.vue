830c055d5791384cfbd19bd747825d52b52966d6
Ollie Hyde <oliver.hyde@wikimedia.de>
2026-09-25 09:45:46 +0100

    Fix indentation in `TabSettings.vue`

--- src/components/Pages/ManageWiki/TabSettings.vue
+++ src/components/Pages/ManageWiki/TabSettings.vue
@@ -1,33 +1,32 @@
 <template>
-          <v-container>
-            <v-row>
-              <v-spacer></v-spacer>
-              <v-col cols="8">
-                <div class="content">
-                  <div class="tab-container">
-                    <div v-if="isReady">
-                      <v-tabs>
-                        <v-tab :to="this.getTabPath('TabSettingsProfileInner')">Profile</v-tab>
-                        <v-tab :to="this.getTabPath('TabSettingsSettingsInner')">Settings</v-tab>
-                        <v-tab :to="this.getTabPath('TabSettingsFeaturesInner')">Features</v-tab>
-                        <v-tab :to="this.getTabPath('TabSettingsReviewInner')">Review</v-tab>
-                      </v-tabs>
-                      <router-view/>
-                    </div>
-                    <div class="text-center" v-if="!isReady">
-                      <br/>
-                      <v-progress-circular indeterminate color="primary"></v-progress-circular>
-                    </div>
-                  </div>
-                </div>
-              </v-col>
-              <v-spacer></v-spacer>
-            </v-row>
-          </v-container>
+  <v-container>
+    <v-row>
+      <v-spacer></v-spacer>
+      <v-col cols="8">
+        <div class="content">
+          <div class="tab-container">
+            <div v-if="isReady">
+              <v-tabs>
+                <v-tab :to="this.getTabPath('TabSettingsProfileInner')">Profile</v-tab>
+                <v-tab :to="this.getTabPath('TabSettingsSettingsInner')">Settings</v-tab>
+                <v-tab :to="this.getTabPath('TabSettingsFeaturesInner')">Features</v-tab>
+                <v-tab :to="this.getTabPath('TabSettingsReviewInner')">Review</v-tab>
+              </v-tabs>
+              <router-view />
+            </div>
+            <div class="text-center" v-if="!isReady">
+              <br />
+              <v-progress-circular indeterminate color="primary"></v-progress-circular>
+            </div>
+          </div>
+        </div>
+      </v-col>
+      <v-spacer></v-spacer>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-
 export default {
   name: 'TabSettings',
   computed: {
